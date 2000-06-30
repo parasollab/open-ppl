7abc7eb71bd950b6d3cdd7d48b39581004e13924
gsong <unknown>
2000-06-30 05:25:43 +0000

    rewrite some functions.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@133 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- OBPRM/util.h
+++ OBPRM/util.h
@@ -8,6 +8,7 @@
 //  Last Modified By:   
 //      08/24/98  Daniel Vallejo
 //      1/13/99   Guang Song
+//	06/29/2000 Guang. rewrite some functions.
 //
 ////////////////////////////////////////////////////
 
@@ -30,51 +31,19 @@ const double BETA_EPSILON = 1.0e-30;
 
 typedef double t44[4][4];
 
-// normalize a value to [0,b]
-inline double Normalize_wrt_b(double a, double b){
-    //double c;
-    //c = a;
-    while(a >= b) a -= b;
-    while (a < 0) a += b;
-    return a;
-}
 
 // Calculate the minimum DIRECTED angular distance between two angles
 // normalized to 1.0
-inline double DirectedAngularDistance(double a,double b)
-{
-    double Mdpos,Mdneg;
-    if(a > b){
-        Mdneg = a-b;
-        Mdpos = 1.0-Mdneg;
-    }
-    else{
-        Mdpos = b-a;
-        Mdneg = 1.0-Mdpos;
-    }
-    Normalize_wrt_b(Mdneg,1.0);
-    Normalize_wrt_b(Mdpos,1.0);
-    if(Mdneg < Mdpos){
-        return -Mdneg;
-    }
-    else{
-        return Mdpos;
-    }
-}
 
-
-void
-WritePathTranformationMatrices(char output_file[80], vector<Cfg> path, Environment *env);
+double DirectedAngularDistance(double a,double b);
 
 
 inline double min(double a, double b){
-    if(a < b) return a;
-    else return b;
+    return a < b ? a : b;
 }
 
 inline double max(double a, double b){
-    if(a > b) return a;
-    else return b;
+    return a > b ? a : b;
 }
 
 // Return the square of a.
@@ -83,5 +52,8 @@ inline double sqr(double a)
     return a*a;
 }
 
+
+void
+WritePathTranformationMatrices(char output_file[80], vector<Cfg> path, Environment *env);
 
 #endif
