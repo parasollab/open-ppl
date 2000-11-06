21787a12f321a7f870b927467b7381c7955d006b
Jyh-Ming Lien <jyh-ming-lien@tamu.edu>
2000-11-06 00:49:06 +0000

    Doxygen Comments
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@278 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- OBPRM/Connection.h
+++ OBPRM/Connection.h
@@ -1,8 +1,10 @@
 // $Id$
 ////////////////////////////////////////////////////////////////////
-//  Connection.h
-//
-//  Created   2/25/98 Aaron Michalk
+/**@file  Connection.h
+  *
+  *@author Aaron Michalk
+  *date  2/25/98 
+  */
 /////////////////////////////////////////////////////////////////////
 
 #ifndef Connection_h
@@ -16,6 +18,13 @@
 class Input;
 class Body;
 
+/**This class stores information about connection for one body to another one.
+  *The information stored in this class includes:
+  *	- Connection type
+  *	- 2 Body instances
+  *	- Tansformation instances
+  *	- DHparameters
+  */
 class Connection {
 public:
     //---------------------------------------------------------------
@@ -26,25 +35,87 @@ public:
 	Prismatic
     };
     //---------------------------------------------------------------
-    //  Constructors and Destructor
+    /**@name  Constructors and Destructor
+      *@todo The documentation of these function are not complete.
+      *I can't really understand what these parameters for.
+      */
     //---------------------------------------------------------------
+    //@{
+    
+    /** Constructor with Body1 and Body2 (Body2 is optional).
+      *Get might be called to retrieve values for unset data member.
+      *@param _body1 Body1.
+      *@param _body2 Body2.
+      */
     Connection(Body * _body1, Body * _body2 = 0);  // Second argument is optional
+    
+    /**You need to provide all information for Constructor.
+      *@param _body1
+      *@param _body1
+      *@param _transformationToBody2
+      *@param _dhparameters
+      *@param _transformationToDHFrame
+      **/
     Connection(Body * _body1, Body * _body2, const Transformation & _transformationToBody2, 
 	 const DHparameters & _dhparameters, const Transformation & _transformationToDHFrame);
+	 
+    ///Destructor
     ~Connection();
+    //@}
+    
     //---------------------------------------------------------------
-    //  Methods
+    /**@name  Access Methods*/
     //---------------------------------------------------------------
+    //@{
+    
+    /**Check if a give body is first body or not.
+      *@param _body A point to Body instance. Address is checked
+      * with the address of first body.
+      *@return True if _body is first body. Otherwise False is returned.
+      */
     int IsFirstBody(Body * _body);
+    
+    /**Get Previouse Body.
+      *@note only body[0] is returned.
+      **/
+    Body * GetPreviousBody();    
+    
+    /**Get Next Body.
+      *@note only body[1] is returned.
+      **/
     Body * GetNextBody();
+    
+    ///Get the type of connection used in this Connection instance.
     ConnectionType GetConnectionType();
-    Body * GetPreviousBody();
+
+    ///Get a refecence of DHparameters used in this Connection instance.   
     DHparameters & GetDHparameters();
+    
+    ///Get a refecence of transformation which is a transformation from body1 to body2. (Not sure)
     Transformation & GetTransformationToBody2();
+    
+    ///Get a refecence of transformation which is a transformation from body1 to DHFrame of body2. (Not sure)
     Transformation & GetTransformationToDHFrame();
+    
+    /** Read values for data member from Input instance.
+      * Get information about Body2, transformationToBody2, and transformationToDHFrame
+      * from a given Input instance.
+      * @param _input Pointer to Input instance. Most information is stored here.
+      * @param _multibodyIndex  Used to find out index of body2 from _input.
+      * @param _connectionIndex Used to find out the type of connection from _input.
+      */
     void Get(Input * _input, int _multibodyIndex, int _connectionIndex);
+    
+    ///Write inforamtion about Body2, transformationToBody2, and transformationToDHFrame to output stream.
     virtual void Write(ostream & _os);
+    //@}
+    
 protected:
+
+///////////////////////////////////////////////////////////////////////////////////////
+// PRIVATE METHOD and DATA MEMBERS
+// NO DOCUMENTATION YET
+///////////////////////////////////////////////////////////////////////////////////////
 private:
     //---------------------------------------------------------------
     //  Data
--- OBPRM/OBPRM.h
+++ OBPRM/OBPRM.h
@@ -1,15 +1,13 @@
 // $Id$
 /////////////////////////////////////////////////////////////////////
-//
-//  OBPRM.h
-//
-//  General Description
-//      This is a set of definitions useful to the OBPRM application
-//	specifically.  Almost all the OBPRM specific files will need 
-//	to include them.
-//
-//  Created
-//      8/25/98  Lucia K. Dale
+/**@file OBPRM.h
+  *This is a set of definitions useful to the OBPRM application specifically.
+  *Almost all the OBPRM specific files will need 
+  *to include them.
+  *
+  *@author Lucia K. Dale
+  *@date  8/25/98
+  */
 /////////////////////////////////////////////////////////////////////
 
 #ifndef OBPRM_h
@@ -23,31 +21,38 @@
 //-----------------------------------
 // Constants
 //---------------------------------
+/** @name Constants used in OBPRM.*/
+//@{
 #define ORIENTATION_RES             0.05
 #define POSITION_RES_FACTOR         0.05
 
-#define NULL_WT_INFO -999              // to pad weight fields for graph conversions
+#define NULL_WT_INFO -999              ///< to pad weight fields for graph conversions
 
 #define INVALID_SID -999
 #define INVALID_EID -999
 
-#define INVALID_DM -999                 // invalid dm id
-#define INVALID_CD -999                 // invalid cd id
-#define INVALID_LP -999                 // invalid lp id
-#define INVALID_GN -999                 // invalid gn id
-#define INVALID_CN -999                 // invalid cn id
+#define INVALID_DM -999                 ///< invalid dm id
+#define INVALID_CD -999                 ///< invalid cd id
+#define INVALID_LP -999                 ///< invalid local planner id
+#define INVALID_GN -999                 ///< invalid generate node id
+#define INVALID_CN -999                 ///< invalid connect node id
+//@}
 
-// NMA: for edge weights... not good values or placement (in BasicDefns?)
+/** @name NMA: for edge weights.
+  * @warning Not good values or placement (in BasicDefns?).
+  */
+//@{
 #define MAX_INT  999999999
 #define INVALID_INT -999
 #define MAX_DBL  999999999
 #define INVALID_DBL -999
+//@}
 
-//-----------------------------------
-// general data structures
-//-----------------------------------
-typedef short EID;  // element id type
-typedef short SID;  // set id type
+/**@name General data structures*/
+//@{
+typedef short SID;///< set id type
+typedef short EID;///< element id type
+//@}
 
 //-----------------------------------
 // c-space representations
@@ -58,45 +63,123 @@ typedef short SID;  // set id type
 // choose represention for edge weights in roadmap graph, some samples
 //-----------------------------------
 
-// "normal" edges 
+/**Represention for edge weights in roadmap graph.
+  *"normal" edges.
+  */
 class IntWeight { 
 public:
+
+  /**@name Constructor and Destructor*/
+  //@{
+  
+  /**Default contructor.
+    *This constructor sets nticks to 1 and other data members to invalid value.
+    */
   IntWeight(){ lp = INVALID_LP; nticks=1; };
+  /**Constrcutor with local planner id.
+    *@param i The ID of local planner??.
+    *@note Set nticks to 1.
+    */
   IntWeight(int i){ lp=i; nticks=1; };
+  
+  /**Construct with local planner id and weight.
+    *@param i The ID of local planner??.
+    *@param j For nticks.
+    */
   IntWeight(int i, int j){ lp=i; nticks=j; };
+  
+  ///Destructor
   ~IntWeight(){};
+  //@}
 
+  /**@name Operator overloading*/
+  //@{
+
+  ///Copy values from antoher given IntWeight instance.
   bool operator== (const IntWeight &tmp) const{return ((lp==tmp.lp)&&(nticks==tmp.nticks)) ;};
+  
+  ///Output values of datamember to given output stream.
   friend ostream& operator<< (ostream& _os, const IntWeight& w);  // in util.c
+  
+  ///Read values of datamember to given input stream.
   friend istream& operator>> (istream& _is, IntWeight& w);        // in util.c
+  //@}
 
+  /**@name Access methods*/
+  //@{
+  
+  /**Create a new IntWeight instance with invalide weight value and return it.
+    *@return A IntWeight instance with invalide weight value.
+    */
   static IntWeight InvalidWeight() { return IntWeight(INVALID_LP); };
-  static double MaxWeight() { return MAX_DBL; }; // for Dijkstra's Alg
+  
+  static double MaxWeight() { return MAX_DBL; }; ///< For Dijkstra's Alg
+  
   double& Weight() { return nticks; };
-
+  
   int& LP() { return lp; };
+  //@}
 
 private:
   int lp;
   double nticks;
 };
 
-// "special" edges, e.g., for protein folding
+/**Represention for edge weights in roadmap graph.
+  *"special" edges, for example, for protein folding.
+  */
 class DblWeight {
 public:
+
+  /**@name Constructor and Destructor*/
+  //@{
+  
+  /**Default contructor.
+    *This constructor sets its data members to invalid value.
+    */
   DblWeight(){ lp=INVALID_LP; weight = INVALID_DBL; };
+  /**Constrcutor with local planner id.
+    *@param i The ID of local planner??.
+    *@note Other datamembers are set to invalide values.
+    */
   DblWeight(int i){ lp=i; weight=INVALID_DBL; };
+  /**Construct with local planner id and weight.
+    *@param i The ID of local planner??.
+    *@param j Weight.
+    */
   DblWeight(int i, double j){ lp=i; weight=j; };
+  
+  ///Destructor
   ~DblWeight(){};
-
+  //@}
+  
+  /**@name Operator overloading*/
+  //@{
+  
+  ///Copy values from antoher given DblWeight instance.
   bool operator== (const DblWeight &tmp) const{return ((lp==tmp.lp)&&(weight==tmp.weight)); };
+  
+  ///Output values of datamember to given output stream.
   friend ostream& operator<< (ostream& _os, const DblWeight& w);  // in util.c
+  
+  ///Read values of datamember to given input stream.
   friend istream& operator>> (istream& _is, DblWeight& w);        // in util.c  
-
+  //@}
+  
+  /**@name Access methods*/
+  //@{
+  
+  /**Create a new DblWeight instance with invalide weight value and return it.
+    *@return A DblWeight instance with invalide weight value.
+    */
   static DblWeight InvalidWeight() { return DblWeight(INVALID_LP); };
-  static double MaxWeight() { return MAX_DBL; }; // for Dijkstra's Alg
+  
+  static double MaxWeight() { return MAX_DBL; }; ///< For Dijkstra's Alg
+  
   double& Weight() { return weight; };
+  
   int& LP() { return lp; };
+  //@}
 
 private:
   int    lp;
