6eacabe72bce6ab8ab70aaec1a3c20733c28b377
gsong <unknown>
2001-02-02 22:47:11 +0000

    add new RMSD function: RMSDwoTranslation()
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@389 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- LigandBinding/MyDistanceMetrics.h
+++ LigandBinding/MyDistanceMetrics.h
@@ -22,6 +22,7 @@ public:
    virtual double Distance(Environment *env, Cfg _c1, Cfg _c2, SID _dmsetid);
    //virtual double RMSD(vector<Vector3D> x, const vector<Vector3D> &y, int);
    virtual double RMSD(vector<Vector3D> x, vector<Vector3D> y, int);
+   virtual double RMSDnoTranslation(vector<Vector3D> x, vector<Vector3D> y, int);
    virtual double WeightedEuclidianDistance(Cfg _c1, Cfg _c2);
 
 };
