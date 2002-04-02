f8dbcb275d72401bc13b1deb2262f1960adba468
Jyh-Ming Lien <jyh-ming-lien@tamu.edu>
2002-04-02 18:27:50 +0000

    fix error on aCC
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@525 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- OBPRM/Input.cpp
+++ OBPRM/Input.cpp
@@ -36,7 +36,7 @@
 #include "CollisionDetection.h"
 
 
-#include <iostream>
+#include <iostream.h>
 //===================================================================
 //  Input class
 //  Constructors and Destructor
