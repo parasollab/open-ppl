eae0925fe77a062932a03a27188f8cb5f19109fa
Shawna Thomas <sthomas@cse.tamu.edu>
2002-07-24 14:22:53 +0000

    Derived class from QueryCmds. Adds new input flag: -checkAllNodes. When -checkAllNodes is set to 1, all nodes in the rdmp are checked to see if they meet the query requirements. When -checkAllNodes is set to 0, only nodes along solution paths are checked to see if they meet the query requirements.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@551 5e2c2758-810a-0410-8eb3-8e4e92d4004e

---
+++ AdaptivePRM/APRM/MyQueryCmds.c
@@ -0,0 +1,88 @@
+/////////////////////////////////////////////////////////////////////
+//  MyQueryCmds.c
+//
+//  Created  07/23/2002  Shawna Thomas
+/////////////////////////////////////////////////////////////////////
+
+#include "MyQueryCmds.h"
+
+#include <iostream.h>
+#include <iomanip.h>
+#include <string.h>
+#include <vector.h>
+#include "util.h"
+
+
+MyQueryCmds::MyQueryCmds() : QueryCmds(), checkAllNodes("-checkAllNodes", 1, 0, 1) {}
+
+
+MyQueryCmds::~MyQueryCmds() {}
+
+
+int
+MyQueryCmds::
+ReadCommandLine(int *argc, char **argv){
+
+  vector<char*> cmd; cmd.reserve(*argc);
+
+  try {
+
+    if (*argc == 1)
+      throw BadUsage();
+
+    cmd.push_back(argv[0]);
+    for (int i=1;i<*argc; ++i) {
+
+      //-- if present then record & remove from command line
+
+        if ( checkAllNodes.AckCmdLine(&i, *argc, argv) ) {
+
+      //-- if unrecognized keep
+
+        } else {
+		cmd.push_back(argv[i]);
+        } //endif
+
+    } //endfor i
+
+  } //endtry
+  catch (BadUsage ) {
+        PrintUsage(cout,argv[0]);
+        exit(-1);
+  } //endcatch
+
+
+  //-- copy (possibly) modified command line back
+  for (int j=0;j<cmd.size(); ++j)
+	argv[j] = cmd[j];
+  *argc = cmd.size();
+
+
+  //-- return (possibly) modified argument count
+  return QueryCmds::ReadCommandLine(argc, argv);
+}
+
+
+void
+MyQueryCmds::
+PrintUsage(ostream& _os,char *executablename){
+  QueryCmds::PrintUsage(_os, executablename);
+
+  cout.setf(ios::left,ios::adjustfield);
+
+  _os << "\n    OPTIONAL:\n";
+  _os << "\n\t"; checkAllNodes.PrintUsage(_os);
+
+  cout.setf(ios::right,ios::adjustfield);
+
+  _os << "\n\n";
+}
+
+
+void
+MyQueryCmds::
+PrintValues(ostream& _os){
+  QueryCmds::PrintValues(_os);
+  _os <<"\n"<<setw(20)<<"checkAllNodes"<<"\t"<<checkAllNodes.GetValue();
+  _os <<"\n\n";
+}
