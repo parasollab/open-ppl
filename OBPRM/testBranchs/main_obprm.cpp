ab33568c31ca8ef3fdd9ec220e0a4f485ef2ade2
Shawna Thomas <sthomas@cse.tamu.edu>
2003-06-27 21:34:17 +0000

    main_obprm.cpp main_query.cpp versions for Cfg_free_tree
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@818 5e2c2758-810a-0410-8eb3-8e4e92d4004e

---
+++ OBPRM/testBranchs/main_obprm.cpp
@@ -0,0 +1,171 @@
+// $Id$
+///////////////////////////////////////////////////////////////////////////////
+//  main_obprm.c		
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream.h>
+
+#include "SwitchDefines.h"
+
+#include "OBPRMDef.h"
+#include "Roadmap.h"
+#include "Input.h"
+
+
+#include "Clock_Class.h"
+#include "Stat_Class.h"
+#include "CollisionDetection.h"
+#include "ConnectMap.h"
+#include "DistanceMetrics.h"
+#include "Weight.h"
+#include "LocalPlanners.h"
+#include "GenerateMapNodes.h"
+
+#include "CfgTypes.h"
+
+typedef Cfg_free_tree CfgType;
+typedef DefaultWeight WeightType;
+
+Input input;
+extern Stat_Class Stats; 
+void PrintRawLine( ostream& _os,
+        Roadmap<CfgType, WeightType> *rmap, Clock_Class *NodeGenClock, Clock_Class *ConnectionClock,
+        int printHeaders);
+
+//========================================================================
+//  main
+//========================================================================
+int main(int argc, char** argv)
+{
+  GenerateMapNodes<CfgType> gn;
+  ConnectMap<CfgType, WeightType> cm;
+  LocalPlanners<CfgType, WeightType> lp;
+  DistanceMetric     dm;
+  CollisionDetection cd;
+  Clock_Class        NodeGenClock;
+  Clock_Class        ConnectionClock;
+
+  //----------------------------------------------------
+  // Get information from the user via the command line
+  //----------------------------------------------------
+  input.ReadCommandLine(argc,argv);
+
+  CfgType::setNumofJoints(input.numofJoints);
+  cout << "Cfg_free_tree::NumofJoints = " 
+       << CfgType::getNumofJoints() << endl;
+
+  // do not seed for now while developing code... ADD SEEDING LATER
+  // srand48((unsigned int) time(NULL));
+
+  //----------------------------------------------------
+  // instantiate roadmap object 
+  //	parse command line and init roadmap, lps, read in environment, etc
+  // initiate collision detection
+  // build map
+  // write map to a file
+  //----------------------------------------------------
+  Roadmap<CfgType, WeightType> rmap(&input,  &cd, &dm, &lp);
+  
+  cd.UserInit(&input,   &gn, &cm );
+  lp.ReadCommandLine(input.LPstrings, input.numLPs, input.cdtype, cm.cdsetid);
+  gn.ReadCommandLine(input.GNstrings, input.numGNs);
+  cm.ReadCommandLine(&input, rmap.GetEnvironment());
+  dm.UserInit(&input,   &gn, &cm );
+
+  #ifdef QUIET
+  #else
+    input.PrintValues(cout);
+  #endif
+
+  if ( input.inmapFile.IsActivated() ){
+    //---------------------------
+    // Read roadmap nodes
+    //---------------------------
+    rmap.ReadRoadmapGRAPHONLY(input.inmapFile.GetValue());
+  } else {
+    //---------------------------
+    // Generate roadmap nodes
+    //---------------------------
+    NodeGenClock.StartClock("Node Generation");
+    vector<CfgType> nodes;
+    gn.GenerateNodes<WeightType>(&rmap,&cd,&dm,nodes);
+    NodeGenClock.StopClock();
+  }
+
+
+  #if QUIET
+  #else
+    cout << "\n";
+    if ( input.inmapFile.IsActivated() ){
+      cout << "Node Generation: "<<argv[argc];
+    }else{
+      cout << "Node Generation: " << NodeGenClock.GetClock_SEC()
+           << " sec (ie, " << NodeGenClock.GetClock_USEC() << " usec)";
+    }
+    
+	cout << ", "<<rmap.m_pRoadmap->GetVertexCount()<<" nodes\n"<< flush;
+  #endif
+
+
+  //---------------------------
+  // Connect roadmap nodes
+  //---------------------------
+  ConnectionClock.StartClock("Node Connection");
+  cm.ConnectComponents(&rmap, &cd, &dm, &lp,
+		       input.addPartialEdge.GetValue(), input.addAllEdges.GetValue());
+  ConnectionClock.StopClock();
+
+  //---------------------------
+  // Write roadmap
+  //---------------------------
+  rmap.WriteRoadmap(&input,&cd,&dm,&lp);
+
+
+  //---------------------------
+  // Print out some useful info
+  //---------------------------
+  #ifdef QUIET
+    ofstream  myofstream(input.mapFile.GetValue(),ios::app);
+    if (!myofstream) {
+      cout<<"\nIn main_obprm: can't re-open mapfile: "
+          <<input.mapFile.GetValue();
+      exit(-1);
+    }
+    PrintRawLine(cout,
+        &rmap, &NodeGenClock,&ConnectionClock,1);  // to stdout
+    PrintRawLine(myofstream,
+        &rmap, &NodeGenClock,&ConnectionClock,0);  // to map
+  #else
+    cout << "\n";
+    ConnectionClock.PrintName();
+    cout << ": " << ConnectionClock.GetClock_SEC()
+         << " sec"
+         << ", "<<rmap.m_pRoadmap->GetEdgeCount()<<" edges\n"<< flush;
+    Stats.PrintAllStats(&rmap);
+  #endif
+
+
+  //------------------------
+  // Done
+  //------------------------
+  #ifdef QUIET
+  #else
+    cout << "\n  !!Bye!! \n";
+  #endif
+  return 0;
+}
+
+
+void PrintRawLine( ostream& _os,
+        Roadmap<CfgType, WeightType> *rmap, Clock_Class *NodeGenClock, Clock_Class *ConnectionClock,
+        int printHeader = 0 ){
+
+  _os << "\nraw ";               // We can grep out "raw" datalines.
+  _os << NodeGenClock->GetClock_SEC()     << " ";
+  _os << NodeGenClock->GetClock_USEC()    << " ";
+  _os << ConnectionClock->GetClock_SEC()  << " ";
+  _os << ConnectionClock->GetClock_USEC() << " ";
+  Stats.PrintDataLine(_os,rmap,printHeader);
+  _os << "\n\n";
+}
