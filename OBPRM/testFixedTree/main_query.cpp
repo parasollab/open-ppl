7ec02b0b809eec65e3a6146f70dfb4d3254d8704
Shawna Thomas <sthomas@cse.tamu.edu>
2003-06-27 21:40:04 +0000

    Example of env for Cfg_fixed_tree
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@823 5e2c2758-810a-0410-8eb3-8e4e92d4004e

---
+++ OBPRM/testFixedTree/main_query.cpp
@@ -0,0 +1,101 @@
+// $Id$
+///////////////////////////////////////////////////////////////////////////////
+//  main_query.c        
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <iostream.h>
+#include "Query.h"
+#include "GenerateMapNodes.h"
+#include "ConnectMap.h"
+#include "LocalPlanners.h"
+#include "DistanceMetrics.h"
+#include "CollisionDetection.h"
+#include "Input.h"
+
+#include "Stat_Class.h"
+#include "Clock_Class.h"
+#include "GraphAlgo.h"
+#include "Weight.h"
+
+#include "CfgTypes.h"
+
+typedef Cfg_fixed_tree CfgType;
+typedef DefaultWeight WeightType;
+
+Input input;
+QueryCmds Qinput;
+extern Stat_Class Stats;
+
+//========================================================================
+//  main
+//========================================================================
+int main(int argc, char** argv)
+{
+  GenerateMapNodes<CfgType>   gn;
+  ConnectMap<CfgType, WeightType> cm;
+  LocalPlanners<CfgType, WeightType> lp;
+  DistanceMetric     dm;
+  CollisionDetection cd;
+  Clock_Class        QueryClock;
+
+  //----------------------------------------------------
+  // instantiate query/roadmap object
+  //    parse command line and init roadmap, lps, read in environment, etc
+  //----------------------------------------------------
+  Qinput.ReadCommandLine(&argc,argv);
+  input.ReadCommandLine(argc,argv);
+
+  CfgType::setNumofJoints(input.numofJoints);
+  cout << "Cfg_fixed_tree::NumofJoints = "
+       << CfgType::getNumofJoints() << endl;
+
+  Query<CfgType, WeightType> query(&input,&Qinput, &cd, &dm, &lp,&cm);
+
+  cd.UserInit(&input,   &gn, &cm );
+  lp.ReadCommandLine(input.LPstrings, input.numLPs, input.cdtype, cm.cdsetid, true);
+  dm.UserInit(&input,   &gn, &cm );
+  gn.ReadCommandLine(input.GNstrings, input.numGNs);
+  cm.ReadCommandLine(&input, query.rdmp.GetEnvironment());
+  
+  /** set up set ids for query stage. And this has been 
+      done after cm has been set up */
+  query.initDefaultSetIDs(&cm);
+
+
+  Qinput.PrintValues(cout);
+
+  //---------------------------
+  // Print out some useful info
+  //---------------------------
+  lp.PrintValues(cout);
+  cout << endl;
+  DisplayCCStats(*(query.rdmp.m_pRoadmap),10);
+  cout << endl;
+
+  //----------------------------------------------------
+  // perform the query
+  // if successful, write path to a file
+  //----------------------------------------------------
+  QueryClock.StartClock("Query");
+  if ( query.PerformQuery(&cd,&cm,&lp,&dm) ) {
+    query.WritePath();
+    cout << endl << "SUCCESSFUL query";
+  } else {
+    cout << endl << "UNSUCCESSFUL query";
+  }
+  QueryClock.StopClock();
+
+  #if QUIET
+  #else
+    cout << ": " << QueryClock.GetClock_SEC()
+         << " sec (ie, " << QueryClock.GetClock_USEC() << " usec)";
+  #endif
+
+
+  //------------------------
+  // Done
+  //------------------------
+  cout << "\n  !!Bye!! \n";
+  return 0;
+}
