507ca176be28d39b6735a599a91774220af50c08
Shawna Thomas <sthomas@cse.tamu.edu>
2003-06-27 21:20:52 +0000

    abstract class to define node generation method interface
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@802 5e2c2758-810a-0410-8eb3-8e4e92d4004e

---
+++ OBPRM/NodeGenerationMethod.h
@@ -0,0 +1,102 @@
+#ifndef NodeGenerationMethod_h
+#define NodeGenerationMethod_h
+
+#include "Parameters.h"
+
+class Environment;
+class CollisionDetection;
+class DistanceMetric;
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+//  class NodeGenerationMethod
+//
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+/**This is the interface for all node generation methods (prm, obprm, maprm, etc.).
+ */
+template <class CFG>
+class NodeGenerationMethod { 
+ public:
+
+  //////////////////////////////////////////////////////////////////////////////////////////
+  //
+  //
+  //    Constructors and Destructor
+  //
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////
+  /**@name Constructors and Destructor*/
+  //@{
+
+  ///Default Constructor.
+  NodeGenerationMethod();
+  ///Destructor.	
+  virtual ~NodeGenerationMethod();
+
+  //@}
+
+  //////////////////////
+  // Access
+  virtual char* GetName() = 0;
+  virtual void SetDefault();
+
+  //////////////////////
+  // I/O methods
+  virtual void ParseCommandLine(int argc, char **argv) = 0;
+  virtual void PrintUsage(ostream& _os) = 0;
+  virtual void PrintValues(ostream& _os) = 0;
+  virtual NodeGenerationMethod<CFG>* CreateCopy() = 0;
+
+
+  /**Generate nodes according to method type, abstract.
+   *@param nodes New created nodes are stored here.
+   */
+  virtual void GenerateNodes(Environment* _env, CollisionDetection* cd, 
+			     DistanceMetric *, vector<CFG>& nodes) = 0;
+
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  //
+  //
+  //    Public Data
+  //
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////
+  ///Number of nodes to generate.
+  num_param<int> numNodes;
+
+  SID* cdsetid;
+  SID* dmsetid;
+  CDInfo* cdInfo;
+};
+
+
+/////////////////////////////////////////////////////////////////////
+//
+//  definitions for class NodeGenerationMethod declarations
+//
+/////////////////////////////////////////////////////////////////////
+template <class CFG>
+NodeGenerationMethod<CFG>::
+NodeGenerationMethod():
+  numNodes         ("nodes",            10,  1,   5000000) {
+  numNodes.PutDesc("INTEGER","(number of nodes, default 10)");
+
+  SetDefault();
+}
+
+
+template <class CFG>
+NodeGenerationMethod<CFG>::
+~NodeGenerationMethod() {
+}
+
+
+template <class CFG>
+void NodeGenerationMethod<CFG>::
+SetDefault() {
+  numNodes.PutValue(10);
+}
+
+#endif
