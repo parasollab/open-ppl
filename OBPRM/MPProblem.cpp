777eb327186e246ee491b5628cc86cd892a61a03
Roger Pearce <rpearce@llnl.gov>
2005-08-23 21:12:30 +0000

    Adding new cpp files for MPProblem and MPStrategy
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@1058 5e2c2758-810a-0410-8eb3-8e4e92d4004e

---
+++ OBPRM/MPProblem.cpp
@@ -0,0 +1,102 @@
+#include "MPProblem.h"
+#include "MPStrategy.h"
+
+MPProblem::
+MPProblem(TiXmlNode* in_pNode) {
+  LOG_MSG("MPProblem::MPProblem()",VERBOSE);
+  
+  ParseXML(in_pNode);
+  rmp.environment = m_pEnvironment;
+}
+
+void MPProblem::
+ParseXML(TiXmlNode* in_pNode) { 
+  LOG_MSG("MPProblem::ParseXML()",VERBOSE);
+  if(!in_pNode) {
+    LOG_MSG("MPProblem::ParseXML() error xml input",ERROR); exit(-1); exit(-1);
+  }
+  if(string(in_pNode->Value()) != "MPProblem") {
+    LOG_MSG("MPProblem::ParseXML() error xml input",ERROR); exit(-1);
+  }
+
+  for( TiXmlNode* pChild = in_pNode->FirstChild(); pChild !=0; 
+       pChild = pChild->NextSibling()) {
+
+    if(string(pChild->Value()) == "file_io") {
+      ParseXMLFileIO(pChild);
+    } else if(string(pChild->Value()) == "environment") {
+      m_pEnvironment = new Environment(pChild, this);
+    } else  if(string(pChild->Value()) == "distance_metrics") {
+      m_pDistanceMetric = new DistanceMetric(pChild);
+    } else  if(string(pChild->Value()) == "collision_detection") {
+      m_pCollisionDetection = new CollisionDetection(pChild);
+    } else  if(string(pChild->Value()) == "MPRegions") {
+      //m_output_dir = string(pChild->ToElement()->Attribute("dir_name"));
+    }else {
+      LOG_MSG("MPProblem::  I don't know: "<< endl << *pChild,WARNING);
+    }
+  }
+
+  PrintOptions();
+  LOG_MSG("~MPProblem::ParseXML()",VERBOSE);
+}
+
+void MPProblem::
+ParseXMLFileIO(TiXmlNode* in_pNode) {
+  LOG_MSG("MPProblem::ParseXMLFileIO()",VERBOSE);
+  if(string(in_pNode->Value()) != "file_io") {
+    LOG_MSG("MPProblem::ParseFileIO() error xml input",ERROR); exit(-1);
+  }
+
+  for( TiXmlNode* pChild = in_pNode->FirstChild(); pChild !=0; 
+       pChild = pChild->NextSibling()) {
+    if(string(pChild->Value()) == "input_env") {
+      m_input_env = string(pChild->ToElement()->Attribute("file_name"));
+    } else if(string(pChild->Value()) == "output_map") {
+      m_output_map = string(pChild->ToElement()->Attribute("file_name"));
+    } else  if(string(pChild->Value()) == "output_dir") {
+      m_output_dir = string(pChild->ToElement()->Attribute("dir_name"));
+    } else {
+      LOG_MSG("MPProblem::  I don't know: "<< endl << *pChild,WARNING);
+    }
+  }
+  LOG_MSG("~MPProblem::ParseXMLFileIO()",VERBOSE);
+}
+
+void MPProblem::
+PrintOptions()
+{
+  cout << "MPProblem" << endl;
+  cout << "  input_env  = " << m_input_env << endl;
+  cout << "  output_map = " << m_output_map << endl;
+  cout << "  output_dir = " << m_output_dir << endl;
+}
+
+void MPProblem::
+WriteRoadmapForVizmo() {
+  LOG_MSG("MPProblem::WriteRoadmapForVizmo()",VERBOSE);
+  ofstream  myofstream(GetOutputRoadmap().c_str());
+  
+  if (!myofstream) {
+    LOG_MSG("MPProblem::WriteRoadmapForVizmo: can't open outfile: ",ERROR);
+    exit(-1);
+  }
+  
+  myofstream << "Roadmap Version Number " << RDMPVER_CURRENT_STR;
+  myofstream << endl << "#####PREAMBLESTART#####";
+  myofstream << endl << "../obprm -f -bbox [-10,10,-10,10,-10,10]";//commandLine;
+  myofstream << endl << "#####PREAMBLESTOP#####";
+  
+  myofstream << endl << "#####ENVFILESTART#####";
+  myofstream << endl << GetEnvFileName();
+  myofstream << endl << "#####ENVFILESTOP#####";
+  
+  m_pMPStrategy->GetLocalPlanners()->WriteLPs(myofstream);
+  GetCollisionDetection()->WriteCDs(myofstream);
+  GetDistanceMetric()->WriteDMs(myofstream);
+  GetRoadmap()->WriteRNGseed(myofstream);
+
+  GetRoadmap()->m_pRoadmap->WriteGraph(myofstream);         // writes verts & adj lists
+  myofstream.close();
+  LOG_MSG("~MPProblem::WriteRoadmapForVizmo()",VERBOSE);
+}
---
+++ OBPRM/MPStrategy.cpp
@@ -0,0 +1,90 @@
+#include "MPStrategy.h"
+
+
+MPStrategy::
+MPStrategy(TiXmlNode* in_pNode, MPProblem* in_pproblem) {
+
+    LOG_MSG( "MPStrategy::MPStrategy()" , VERBOSE);
+    m_pProblem = in_pproblem;
+    addPartialEdge= true;
+    addAllEdges=true;
+    if(!in_pNode) {
+      LOG_MSG("MPStrategy::MPStrategy() error xml input",ERROR); exit(-1);
+    }
+    if(string(in_pNode->Value()) != "MPStrategy") {
+      LOG_MSG("MPStrategy::MPStrategy() error xml input",ERROR); exit(-1);
+    }
+ 
+    for( TiXmlNode* pChild = in_pNode->FirstChild(); pChild !=0; pChild = pChild->NextSibling()) {
+      if(string(pChild->Value()) == "node_generation_methods") {
+        m_pNodeGeneration = new GenerateMapNodes<CfgType>(pChild);
+      } else if(string(pChild->Value()) == "connection_methods") {
+        m_pConnection = new ConnectMap<CfgType, WeightType>(pChild);
+      } else if(string(pChild->Value()) == "lp_methods") {
+        m_pLocalPlanners = new LocalPlanners<CfgType, WeightType>(pChild);
+      } else {
+        LOG_MSG("MPStrategy::  I don't know: "<< endl << *pChild,WARNING);
+      }
+    }
+    LOG_MSG( "~MPStrategy::MPStrategy()" , VERBOSE);
+}
+
+
+void MPStrategy::
+GenerateMap() {
+  LOG_MSG("MPStrategy::GenerateMap()",VERBOSE);
+  Stat_Class Stats;
+  
+  Clock_Class        NodeGenClock;
+  Clock_Class        ConnectionClock;
+  vector<CfgType> nodes;
+  nodes.erase(nodes.begin(),nodes.end());
+
+  //---------------------------
+  // Generate roadmap nodes
+  //---------------------------
+  NodeGenClock.StartClock("Node Generation");
+  cout << "Starting Generate Nodes" << endl;
+  m_pNodeGeneration->GenerateNodes(m_pProblem->GetRoadmap(),Stats,
+                                    m_pProblem->GetCollisionDetection(),
+                                    m_pProblem->GetDistanceMetric(),nodes);
+  cout << "Finished Generate Nodes" << endl;
+  NodeGenClock.StopClock();
+
+
+  //---------------------------
+  // Connect roadmap nodes
+  //---------------------------
+  ConnectionClock.StartClock("Node Connection");
+  cout << "Starting connect" << endl;
+  m_pConnection->Connect(m_pProblem->GetRoadmap(), Stats, 
+                          m_pProblem->GetCollisionDetection(),
+                          m_pProblem->GetDistanceMetric(), 
+                          m_pLocalPlanners,
+                          addPartialEdge, addAllEdges);
+  cout << "Finished connect" << endl;
+  ConnectionClock.StopClock();
+  /*
+  Input* input = new Input;
+  cout << "Writing roadmap" << endl;
+
+  char env[100];
+  char cmd[100];
+  strcpy(env,m_pProblem->GetEnvFileName().c_str());
+  input->envFile.SetValue(env);
+  strcpy(cmd,"../obprm -f");
+  cout << "setting envfile to: " << env << endl;
+  strcpy(input->commandLine,cmd);
+  */
+  /*
+  m_pProblem->GetRoadmap()->WriteRoadmap(input,m_pProblem->GetCollisionDetection(),
+                               m_pProblem->GetDistanceMetric(), 
+                               m_pLocalPlanners,
+                               m_pProblem->GetOutputRoadmap());
+  */
+  m_pProblem->WriteRoadmapForVizmo();
+  
+  //delete input;
+  cout << "Finished writting roadmap" << endl;
+  LOG_MSG("~MPStrategy::GenerateMap()",VERBOSE);
+} 
