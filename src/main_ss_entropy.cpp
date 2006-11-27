a5e55cb5c6135c35bfa6674401df8ada3b7d78a1
Shawna Thomas <sthomas@cse.tamu.edu>
2006-11-27 20:48:16 +0000

    MERGE 1399:13999 EntropyPRM to trunk
    added -DPMPCfg2D
    M            1399   GNUmakefile
    M            1399   Cfg/CfgTypes.h
    
    added new connection method: k unconnected closest
    ?                   Connector/ClosestUnconnected.h
    M            1399   Connector/ConnectMap.h
    
    added seed input flag
    M            1399   trash/Input.cpp
    M            1399   trash/Input.h
    M            1399   main_obprm.cpp
    
    added new strategy: HybridPRM
    ?                   MPStrategy/HybridPRM.h
    ?                   MPStrategy/HybridPRM.cpp
    M            1399   MPProblem/RoadmapGraph.h
    M            1399   MPStrategy/MPStrategy.h
    M            1399   MPStrategy/MPStrategy.cpp
    M	     1399   GNUMakefile.OBPRMobjs
    
    removed unnecessary includes, changed to forward declarations
    M            1399   MapEvaluator/MPRegionComparerMethod.h
    M            1399   MapEvaluator/MapEvaluator.h
    M            1399   MapEvaluator/MPCharacterizer.h
    M            1399   MPProblem/Boundary.h
    M            1399   MPProblem/Boundary.cpp
    M            1399   MPProblem/GeneratePartitions.h
    M            1399   MPProblem/MPProblem.h
    M            1399   MPProblem/MPRegion.h
    M            1399   MPStrategy/MPStrategyMethod.h
    
    optimized lp cache
    M            1399   MPProblem/Roadmap.h
    
    added new functionality to PRMIncrementalStrategy
    M            1399   MPStrategy/PRMIncrementalStrategy.cpp
    M            1399   MPStrategy/PRMIncrementalStrategy.h
    
    this->, calls KClosest
    M            1399   MPStrategy/RRTexpand.h
    
    added new Strategy: UtilityGuidedGeneration
    ?                   MPStrategy/UtilityGuidedGenerator.h
    ?                   main_utility_guided.cpp
    
    added new node gen method: PRMActiveLearning
    ?                   NodeGenerator/PRMActiveLearning.h
    M	     1399   NodeGenerator/GenerateMapNodes.h
    
    changed cd callee string
    M            1399   NodeGenerator/MAPRMCSpace.h
    
    uses lp cache
    M            1399   Connector/ConnectCCs.h
    
    ScaleCfg, KClosestByIndex, timing information
    M            1399   DistanceMetric/DistanceMetrics.cpp
    M            1399   DistanceMetric/DistanceMetrics.h
    
    GetRandomRay, ApproxClearance 
    M            1399   Cfg/Cfg.cpp
    M            1399   Cfg/Cfg_free.cpp
    M            1399   Cfg/Cfg_fixed_PRR.cpp
    M            1399   Cfg/Cfg_fixed_tree.h
    M            1399   Cfg/Cfg.h
    M            1399   Cfg/Cfg_free.h
    M            1399   Cfg/Cfg_fixed_PRR.h
    M            1399   Cfg/Cfg_free_tree.cpp
    M            1399   Cfg/Cfg_free_tree.h
    M            1399   Cfg/Cfg_2D.cpp
    M            1399   Cfg/Cfg_2D.h
    M            1399   Cfg/Cfg_fixed_tree.cpp
    M            1399   NodeGenerator/PRMBridgeTest.h
    M            1399   NodeGenerator/MAPRMWSpace.h
    M            1399   NodeGenerator/OBPRM.h
    M            1399   NodeGenerator/BasicOBPRM.h
    M            1399   NodeGenerator/PRMGauss.h
    M            1399   LocalPlanner/ApproxSpheres.h
    
    BBox
    M            1399   MPProblem/Geometry/Body.h
    M            1399   MPProblem/Geometry/Body.cpp
    
    Entropy methods
    ?                   MPStrategy/Entropy/EntropyPRM.h
    ?                   MPStrategy/Entropy/EntropyRegionSet.h
    ?                   MPStrategy/Entropy/EntropyModel.h
    ?                   MPStrategy/Entropy/EntropyRegion.h
    A               0   MPStrategy/Entropy
    ?                   main_mq_entropy.cpp
    ?                   main_ss_entropy.cpp
    M            1399   GNUmakefile.OBPRMdefaults
    
    
    
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@1401 5e2c2758-810a-0410-8eb3-8e4e92d4004e

---
+++ src/main_ss_entropy.cpp
@@ -0,0 +1,155 @@
+///////////////////////////////////////////////////////////////////////////////
+//  main_obprm.c		
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "SwitchDefines.h"
+#include "OBPRMDef.h"
+#include "Roadmap.h"
+#include "Input.h"
+#include "Clock_Class.h"
+#include "Stat_Class.h"
+#include "CollisionDetection.h"
+#include "ConnectMap.h"
+#include "DistanceMetrics.h"
+#include "Weight.h"
+#include "LocalPlanners.h"
+#include "CfgTypes.h"
+#include "EntropyPRM.h"
+
+#include "Query.h"
+
+
+Input input;
+QueryCmds Qinput; //sam
+Stat_Class Stats; 
+
+//========================================================================
+//  main
+//========================================================================
+int main(int argc, char** argv)
+{
+  EntropyPRM<CfgType> eprm;
+  LocalPlanners<CfgType, WeightType> lp;
+  DistanceMetric     dm;
+  CollisionDetection cd;
+  CDInfo cdInfo;
+
+  //---------------------------------------------------
+  // Get information from the user via the command line
+  //----------------------------------------------------
+  Qinput.ReadCommandLine(&argc,argv); //sam
+  input.ReadCommandLine(argc,argv);
+
+ CfgType::setNumofJoints(input.numofJoints.GetValue());
+   cout << "Cfg_free_tree::NumofJoints = " 
+       << CfgType::getNumofJoints() << endl;
+
+  long baseSeed;
+  //baseSeed = OBPRM_srand();
+  baseSeed = OBPRM_srand((unsigned int) time(NULL));
+
+  //----------------------------------------------------
+  // instantiate roadmap object 
+  //	parse command line and init roadmap, lps, read in environment, etc
+  // initiate collision detection
+  // build map
+  // write map to a file
+  //----------------------------------------------------
+
+  // create new environment
+  CfgType test_cfg;
+  Environment env(test_cfg.DOF(),test_cfg.posDOF(), &input);
+ 
+  //create new roadmap
+  Roadmap<CfgType, WeightType> rmap(&input, &cd, &dm, &lp, baseSeed, &env);
+  Roadmap<CfgType, WeightType> regionMap(&input, &cd, &dm, &lp, baseSeed, &env);
+  cout << "the RNGseed in rmap is (from main): " << rmap.GetRNGseed() << endl;
+
+  cd.ReadCommandLine(input.CDstrings, input.numCDs);
+  lp.ReadCommandLine(input.LPstrings, input.numLPs, input.cdtype);
+  eprm.ReadCommandLine(input.GNstrings, input.numGNs);
+  //eprm.cm.ReadCommandLine(&input, &env);
+  dm.ReadCommandLine(input.DMstrings, input.numDMs);
+
+  #ifdef QUIET
+  #else
+    input.PrintValues(cout);
+  #endif
+
+  Clock_Class MapGenClock;
+  MapGenClock.StartClock("Map Generation");
+  vector<CfgType> nodes;
+
+  Clock_Class GenClock;
+  GenClock.StartClock("Node Generation");
+  eprm.GenerateNodes(&env, Stats, &cd, &cdInfo, &dm, nodes);
+  rmap.m_pRoadmap->AddVertex(nodes);
+  GenClock.StopPrintClock();
+  eprm.cmodel.WriteRegionsToSpecFile("regions.spec");
+  eprm.cmodel.WriteRegionsToMapFile(&regionMap, &input, &cd, &dm, &lp);
+
+  Clock_Class MergeRegionClock;
+  MergeRegionClock.StartClock("Merging Regions");
+  eprm.cmodel.MergeRegions(&env, 
+			   Stats, &cd, &cdInfo, &dm);
+  MergeRegionClock.StopPrintClock();
+
+/*
+  Clock_Class RegionClock;
+  RegionClock.StartClock("Merging Regions");
+  eprm.cmodel.BuildRegionMap(&env, &dm);
+  eprm.cmodel.MergeRegions(&env, Stats, &cd, &cdInfo, &dm);
+  RegionClock.StopPrintClock();
+  eprm.cmodel.WriteRegionsToSpecFile("merged.spec");
+*/
+
+/*
+  Clock_Class ConnClock;
+  ConnClock.StartClock("NodeConnection");
+  eprm.Connect(&rmap, Stats, &cd, &cdInfo, &dm, &lp, 
+	       input.addPartialEdge.GetValue(), input.addAllEdges.GetValue(),
+	       5, 10);
+  ConnClock.StopPrintClock();
+*/
+
+  //query used in lazy style
+  Query<CfgType, WeightType> query(&Qinput);//sam
+  CfgType s_cfg = query.getStartCFG();
+  CfgType g_cfg = query.getGoalCFG();
+  cout << " start config: " << s_cfg << endl;
+  cout << " goal config: " << g_cfg << endl;
+  eprm.ExtractRegionPath(&rmap,s_cfg, g_cfg, Stats, &cd, &cdInfo, &dm, &lp);
+
+  MapGenClock.StopClock();
+
+  
+  //---------------------------
+  // Write roadmap
+  //---------------------------
+  rmap.WriteRoadmap(&input,&cd,&dm,&lp);
+
+
+  //---------------------------
+  // Print out some useful info
+  //---------------------------
+  #ifndef QUIET
+    cout << "\n";
+    MapGenClock.PrintName();
+    cout << ": " << MapGenClock.GetClock_SEC()
+         << " sec"
+ 	 << ", "<<rmap.m_pRoadmap->GetVertexCount()<<" nodes"
+         << ", "<<rmap.m_pRoadmap->GetEdgeCount()<<" edges\n"<< flush;
+    Stats.PrintAllStats(&rmap);
+  #endif
+
+
+  //------------------------
+  // Done
+  //------------------------
+  #ifdef QUIET
+  #else
+    cout << "\n  !!Bye!! \n";
+  #endif
+  return 0;
+}
