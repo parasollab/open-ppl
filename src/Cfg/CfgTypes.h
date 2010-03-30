55dc86974527627fb2d6ee82b05e01d47d270e2c
Shawna Thomas <sthomas@cse.tamu.edu>
2010-03-30 15:34:52 +0000

    cleaned up includes, added definition for folding code
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@2450 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/Cfg/CfgTypes.h
+++ src/Cfg/CfgTypes.h
@@ -1,56 +1,61 @@
 #ifndef CfgTypes_h
 #define CfgTypes_h
 
-#include "Cfg_free.h"
-#include "Cfg_2D.h"
-#include "Cfg_free_tree.h"
-#include "Cfg_free_tree_2dof.h"
-#include "Cfg_fixed_tree.h"
-#include "Cfg_fixed_PRR.h"
-#include "Cfg_reach_cc.h"
-#include "Cfg_reach_cc_fixed.h"
-#include "Cfg_free_multi.h"
-
 #include "Weight.h"
 
 #ifdef PMPRigid
+#include "Cfg_free.h"
 typedef Cfg_free CfgType;
 typedef DefaultWeight WeightType;
 #endif
 
 #ifdef PMPSerial
+#include "Cfg_free_tree.h"
 typedef Cfg_free_tree CfgType;
 typedef DefaultWeight WeightType;
 #endif
 
 #ifdef PMPFixedTree
+#include "Cfg_fixed_tree.h"
 typedef Cfg_fixed_tree CfgType;
 typedef DefaultWeight WeightType;
 #endif
 
 #ifdef PMPCfg2D
+#include "Cfg_2D.h"
 typedef Cfg_2D CfgType;
 typedef DefaultWeight WeightType;
 #endif
 
 #ifdef PMPReachDistCC
+#include "Cfg_reach_cc.h"
 typedef Cfg_reach_cc CfgType;
 typedef DefaultWeight WeightType;
 #endif
 
 #ifdef PMPReachDistCCFixed
+#include "Cfg_reach_cc_fixed.h"
 typedef Cfg_reach_cc_fixed CfgType;
 typedef DefaultWeight WeightType;
 #endif
 
 #ifdef PMPSerial2DOF
+#include "Cfg_free_tree_2dof.h"
 typedef Cfg_free_tree_2dof CfgType;
 typedef DefaultWeight WeightType;
 #endif
 
 #ifdef PMPRigidMulti
+#include "Cfg_free_multi.h"
 typedef Cfg_free_multi CfgType;
 typedef DefaultWeight WeightType;
+#endif
+
+#ifdef PMPProtein
+#include "CfgProtein.h"
+#include "BioWeight.h"
+typedef CfgProtein CfgType;
+typedef BioWeight WeightType;
 #endif
 
 #endif
