71f9fe8503871514779c701ef5d9bfa27738d082
Chinwe Ekenna <cekenna@cse.tamu.edu>
2010-11-15 20:20:24 +0000

    Changes to the kclosest function in BFFNF
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@2684 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/NeighborhoodFinder/BFFNF.hpp
+++ src/NeighborhoodFinder/BFFNF.hpp
@@ -11,18 +11,7 @@ using namespace std;
 
 /**Compare two distances in DIST_TYPE instances.
  *return (_cc1.second < _cc2.second)
- */
-template <class T>
-class T_DIST_Compare2 : public binary_function<const pair<T,double>,
-              const pair<T,double>,
-              bool> {
- public:
-  bool operator()(const pair<T,double> _cc1,
-      const pair<T,double> _cc2) {
-    return (_cc1.second < _cc2.second);
-  }
-};
-
+*/
 
 template<typename CFG, typename WEIGHT>
 class BFFNF: public NeighborhoodFinderMethod {
@@ -37,7 +26,7 @@ public:
     dmm2 = in_pProblem->GetDistanceMetric()->GetDMMethod(dm2_label);
     
       nf1 = new BFNF<CFG,WEIGHT>(dmm);
-    nf2 = new BFNF<CFG,WEIGHT>(dmm2);
+      nf2 = new BFNF<CFG,WEIGHT>(dmm2);
   }
  BFFNF(shared_ptr<DistanceMetricMethod> _dmm,shared_ptr<DistanceMetricMethod>_dmm2,int _k2) :
     NeighborhoodFinderMethod() {
@@ -99,7 +88,7 @@ public:
   KClosestPairs( Roadmap<CFG,WEIGHT>* _rmp,
     InputIterator _in1_first, InputIterator _in1_last, 
     InputIterator _in2_first, InputIterator _in2_last, 
-    int k, OutputIterator _out) {};
+    int k, OutputIterator _out);
 
 
 private:
@@ -116,7 +105,7 @@ BFFNF<CFG,WEIGHT>::
 KClosest( Roadmap<CFG,WEIGHT>* _rmp, 
     InputIterator _input_first, InputIterator _input_last, VID _v,
     int k, OutputIterator _out) {
-
+ return KClosest(_rmp, _input_first, _input_last, (_rmp->m_pRoadmap->find_vertex(_v))->property(), k, _out);
 
 }
 
@@ -128,8 +117,13 @@ KClosest( Roadmap<CFG,WEIGHT>* _rmp,
   InputIterator _input_first, InputIterator _input_last, CFG _cfg, 
   int k, OutputIterator _out) {
 
+ vector<VID> closest;
 
-  
+   // typename vector<VID>::iterator myint;
+    back_insert_iterator<vector<VID> > b_iter = nf1->KClosest(_rmp,_cfg,m_scale,back_insert_iterator<vector<VID> >(closest));
+          //copy(closest.begin(), closest.end(),b_iter);
+           return nf2->KClosest(_rmp,closest.begin(),closest.end(),_cfg,k,_out);
+   
 }
 
 template<typename CFG, typename WEIGHT>
@@ -138,13 +132,7 @@ OutputIterator
 BFFNF<CFG,WEIGHT>::
 KClosest( Roadmap<CFG,WEIGHT>* _rmp,
   VID _v, int k, OutputIterator _out) {
-cout<<"calling the 2nd one"<<endl;
-  RoadmapGraph<CFG,WEIGHT>* pMap = _rmp->m_pRoadmap;
- // CFG _v_cfg = pMap->find_vertex(_v).property();
-  CFG _v_cfg = (*(pMap->find_vertex(_v))).property();
-  return KClosest(_rmp, _v_cfg, k, _out);
-  
- ;
+return KClosest(_rmp, (_rmp->m_pRoadmap->find_vertex(_v))->property(), k, _out);
 }
 
 template<typename CFG, typename WEIGHT>
@@ -154,13 +142,17 @@ BFFNF<CFG,WEIGHT>::
 KClosest( Roadmap<CFG,WEIGHT>* _rmp,
   CFG _cfg, int k, OutputIterator _out) {
 
-    vector<VID> closest;
- 
-   // typename vector<VID>::iterator myint;
-   back_insert_iterator<vector<VID> > b_iter = nf1->KClosest(_rmp,_cfg,m_scale,back_insert_iterator<vector<VID> >(closest));
-   //copy(closest.begin(), closest.end(),b_iter);
-   return nf2->KClosest(_rmp,closest.begin(),closest.end(),_cfg,k,_out);
+return KClosest(_rmp, _rmp->m_pRoadmap->descriptor_begin(), _rmp->m_pRoadmap->descriptor_end(), _cfg, k, _out);
 
+}
+template<typename CFG, typename WEIGHT>
+template<typename InputIterator, typename OutputIterator>
+OutputIterator
+BFFNF<CFG,WEIGHT>::
+KClosestPairs( Roadmap<CFG,WEIGHT>* _rmp,
+  InputIterator _in1_first, InputIterator _in1_last,
+  InputIterator _in2_first, InputIterator _in2_last,
+  int k, OutputIterator _out) {
 }
 
 #endif //end #ifndef _BRUTE_FORCE_FIND_NEIGHBORHOOD_FINDER_H_
