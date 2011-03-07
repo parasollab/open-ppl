fefad62cc983e2b05e0eaa9524af9b0df5cffa72
Chinwe Ekenna <cekenna@cse.tamu.edu>
2011-03-07 20:16:43 +0000

    bug fixed in the cfg_reachable code to properly handle open chaing problems
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@2733 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/Cfg/Cfg_reach_cc_fixed.cpp
+++ src/Cfg/Cfg_reach_cc_fixed.cpp
@@ -9,24 +9,29 @@ Cfg_reach_cc_fixed() : Cfg_reach_cc() {
 
 Cfg_reach_cc_fixed::
 Cfg_reach_cc_fixed(int _numofjoints) : Cfg_reach_cc(_numofjoints) {
+   
 }
 
 Cfg_reach_cc_fixed::
 Cfg_reach_cc_fixed(const Vector6D& _v) : Cfg_reach_cc(_v) {
+  
 }
 
 Cfg_reach_cc_fixed::
 Cfg_reach_cc_fixed(const vector<double>& _v) : Cfg_reach_cc(_v) {
+  
 }
 
 Cfg_reach_cc_fixed::
 Cfg_reach_cc_fixed(const Cfg& c) : Cfg_reach_cc(c) {
+   
 }
 
 Cfg_reach_cc_fixed::
 Cfg_reach_cc_fixed(double x, double y, double z, 
 	     double roll, double pitch, double yaw) 
   : Cfg_reach_cc(x, y, z, roll, pitch, yaw) {
+    
 }
 
 Cfg_reach_cc_fixed::
@@ -42,11 +47,16 @@ GetRandomCfg(double R, double rStep) {
 void 
 Cfg_reach_cc_fixed::
 GetRandomCfg_CenterOfMass(Environment* env) {
+  
   for(int i=0; i<6; ++i)
     v[i] = 0;
 
   link_tree->ResetTree();
-  link_tree->RecursiveSample(0, true, gamma);
+   if(is_closed_chain)
+    link_tree->RecursiveSample(0, true, gamma);
+  else
+    link_tree->RecursiveSample(-1, true, gamma);
+ // link_tree->RecursiveSample(0, true, gamma);
   link_lengths.clear();
   link_orientations.clear();
   link_tree->ExportTreeLinkLength(link_lengths, link_orientations);
@@ -74,12 +84,13 @@ CreateNewCfg() const {
 Cfg* 
 Cfg_reach_cc_fixed::
 CreateNewCfg(vector<double>& _v) const {
-  if(_v.size() < dof) {
+  if((int)_v.size() < dof) {
     cout << "\n\nERROR in Cfg_reach_cc_fixed::CreateNewCfg(vector<double>), ";
     cout << "size of vector is less than dof\n";
     exit(-1);
   }
   vector<double> _data(_v.begin(), _v.begin()+dof);
   Cfg* tmp = new Cfg_reach_cc_fixed(_data);
+ 
   return tmp;
 }
