f857766fb81f40da6d5966f2089406ba18de9a53
Chinwe Ekenna <cekenna@cse.tamu.edu>
2011-08-10 19:34:27 +0000

    made changes to the VIZMDebugOutput update to accomodate the folding code .. Verified by Jory
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@2885 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/Utilities/VizmoDebugOutput.cpp
+++ src/Utilities/VizmoDebugOutput.cpp
@@ -1,5 +1,4 @@
 #include "VizmoDebugOutput.h"
-#include <fstream>
 
 ofstream* vdo = NULL;
 
@@ -7,7 +6,7 @@ void VDInit(string filename){
   if(filename!=""){
     vdo = new ofstream(filename.c_str());
   }
-}
+};
 
 void VDClose(){
   if(vdo!=NULL){
@@ -16,74 +15,28 @@ void VDClose(){
   }
 }
 
-void VDAddNode(CfgType cfg){
-  if(vdo!=NULL){
-    (*vdo) << "AddNode " << cfg << endl;
-  }
-}
-
-void VDRemoveNode(CfgType cfg){
-  if(vdo!=NULL){
-    (*vdo) << "RemoveNode " << cfg << endl;
-  }
-}
-
-void VDAddEdge(CfgType cfg1, CfgType cfg2){
-  if(vdo!=NULL){
-    (*vdo) << "AddEdge " << cfg1 << " " << cfg2 << endl;
-  }
-}
-
-void VDRemoveEdge(CfgType cfg1, CfgType cfg2){
-  if(vdo!=NULL){
-    (*vdo) << "RemoveEdge " << cfg1 << " " << cfg2 << endl;
-  }
-}
-
-void VDAddTempCfg(CfgType cfg, bool valid){
-  if(vdo!=NULL){
-    (*vdo) << "AddTempCfg " << cfg << " " << valid << endl;
-  }
-}
-
-void VDAddTempRay(CfgType cfg){
-  if(vdo!=NULL){
-    (*vdo) << "AddTempRay " << cfg << endl;
-  }
-}
-
-void VDAddTempEdge(CfgType cfg1, CfgType cfg2){
-  if(vdo!=NULL){
-    (*vdo) << "AddTempEdge " << cfg1 << " " << cfg2 << endl;
-  }
-}
-
 void VDComment(string s){
   if(vdo!=NULL){
     (*vdo) << "Comment " << s << endl;
   }
-}
+};
 
 void VDClearAll(){
   if(vdo!=NULL){
     (*vdo) << "ClearAll " << endl;
   }
-}
+};
 
 void VDClearLastTemp(){
   if(vdo!=NULL){
     (*vdo) << "ClearLastTemp " << endl;
   }
-}
+};
 
 void VDClearComments(){
   if(vdo!=NULL){
     (*vdo) << "ClearComments " << endl;
   }
-}
+};
 
-void VDQuery(CfgType cfg1, CfgType cfg2){
-  if(vdo!=NULL){
-    (*vdo) << "Query " << cfg1 << " " << cfg2 << endl;
-  }
-}
+;
--- src/Utilities/VizmoDebugOutput.h
+++ src/Utilities/VizmoDebugOutput.h
@@ -1,26 +1,80 @@
 #ifndef VIZMODEBUGOUTPUT_H_
 #define VIZMODEBUGOUTPUT_H_
 
+#include <fstream>
 #include <iostream>
-#include "Cfg.h"
-#include "CfgTypes.h"
+#include <string>
 
 using namespace std;
 
-void VDInit(string filename);
-void VDClose();
-void VDAddNode(CfgType cfg);
-void VDRemoveNode(CfgType cfg);
-void VDAddEdge(CfgType cfg1, CfgType cfg2);
-void VDRemoveEdge(CfgType cfg1, CfgType cfg2);
-void VDAddTempCfg(CfgType cfg, bool valid = true);
-void VDAddTempRay(CfgType cfg);
-void VDAddTempEdge(CfgType cfg1, CfgType cfg2);
-void VDComment(string s);
-void VDClearAll();
-void VDClearLastTemp();
-void VDClearComments();
-void VDQuery(CfgType cfg1, CfgType cfg2);
+extern ofstream* vdo;
 
+void VDInit(string filename);
+
+void VDClose();
+
+template<class CFG>
+void VDAddNode(CFG cfg){
+  if(vdo!=NULL){
+    (*vdo) << "AddNode " << cfg << endl;
+  }
+};
+
+template<class CFG>
+void VDRemoveNode(CFG cfg){
+  if(vdo!=NULL){
+    (*vdo) << "RemoveNode " << cfg << endl;
+  }
+};
+
+template<class CFG>
+void VDAddEdge(CFG cfg1, CFG cfg2){
+  if(vdo!=NULL){
+    (*vdo) << "AddEdge " << cfg1 << " " << cfg2 << endl;
+  }
+};
+
+template<class CFG>
+void VDRemoveEdge(CFG cfg1, CFG cfg2){
+  if(vdo!=NULL){
+    (*vdo) << "RemoveEdge " << cfg1 << " " << cfg2 << endl;
+  }
+};
+
+template<class CFG>
+void VDAddTempCfg(CFG cfg, bool valid){
+  if(vdo!=NULL){
+    (*vdo) << "AddTempCfg " << cfg << " " << valid << endl;
+  }
+};
+
+template<class CFG>
+void VDAddTempRay(CFG cfg){
+  if(vdo!=NULL){
+    (*vdo) << "AddTempRay " << cfg << endl;
+  }
+};
+
+template<class CFG>
+void VDAddTempEdge(CFG cfg1, CFG cfg2){
+  if(vdo!=NULL){
+    (*vdo) << "AddTempEdge " << cfg1 << " " << cfg2 << endl;
+  }
+};
+
+void VDComment(string s);
+
+void VDClearAll();
+
+void VDClearLastTemp();
+
+void VDClearComments();
+
+template<class CFG>
+void VDQuery(CFG cfg1, CFG cfg2){
+  if(vdo!=NULL){
+    (*vdo) << "Query " << cfg1 << " " << cfg2 << endl;
+  }
+};
 
 #endif
