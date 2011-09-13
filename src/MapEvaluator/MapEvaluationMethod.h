8614ed4efb3ada4e113f8c5cd859b8cd5eb8456e
Shawna Thomas <sthomas@cse.tamu.edu>
2011-09-13 01:56:52 +0000

    added method bool HasState() const that indicates whether or not the evaluator class contains state information that need to be recomputed/reinitialized when starting computation from an existing roadmap; the default is false
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@2907 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/MapEvaluator/MapEvaluationMethod.h
+++ src/MapEvaluator/MapEvaluationMethod.h
@@ -15,6 +15,12 @@ class MapEvaluationMethod
   
   virtual void PrintOptions(ostream& out_os) = 0;
   
+  //has_state is called by strategies that start from an existing roadmap
+  //if has_state returns true, then the evaluator is called on the input
+  //roadmap to reset the state of the object
+  //note that most evaluators do not have state, so this is set to false by default
+  virtual bool HasState() const { return false; }
+
   virtual bool operator() () = 0;
   virtual bool operator() (int in_RegionID) = 0;
 };
