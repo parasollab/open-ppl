e52fef45ec17bdd60135b3094e16c6ae83050479
Chinwe Ekenna <cekenna@cse.tamu.edu>
2012-04-02 19:20:29 +0000

    updated version of the knot theory distance metric
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@3216 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/DistanceMetric/KnotTheoryDistance.cpp
+++ src/DistanceMetric/KnotTheoryDistance.cpp
@@ -14,43 +14,33 @@ KnotTheoryDistance::~KnotTheoryDistance(){
 }
 
 double KnotTheoryDistance ::Distance(Environment* _env, const Cfg& _c1, const Cfg& _c2){
-  double sum =0.0, sign = 0,sign2 =0, temp1,dist,nn;
+  double sum =0.0, sign = 0,sign2 =0,dist,area;
   dist =EuclideanDistance::Distance(_env, _c1,_c2);
-  Vector3D n1,n2;
-  vector <Vector3D> unitVect(3000), unitVect2(3000), c11, c21;
+  vector <Vector3D> unitVect(3000),unitVect2(3000);
   vector<Vector3D> c1=_c1.PolyApprox(_env);
   vector<Vector3D> c2=_c2.PolyApprox(_env);
-  if (c1.empty()|| c1.size()<2){
+  
+   if (c1.empty()|| c1.size()<2){
     cerr << "\n\nError in KnotTheoryDistance::Distance(), c1 has too few links, exiting.\n";
     exit(-1);
   }
   for(size_t i =0; i < c1.size()-1;i++){
     if((c1[i+1]-c1[i]).magnitude() != 0)
-      unitVect[i] = (c1[i+1]+c1[i]);
+      unitVect[i] = (c1[i+1]+c1[i])/(c1[i+1]-c1[i]).magnitude();
     else
       unitVect[i] = 0;
     if((c2[i+1]-c2[i]).magnitude() !=0)
-      unitVect2[i]= (c2[i+1]+c2[i]);
+      unitVect2[i]= (c2[i+1]+c2[i])/(c2[i+1]-c2[i]).magnitude();
     else 
       unitVect2[i]=0;
-    if((unitVect[i].crossProduct(unitVect2[i])).magnitude() != 0)
-      n1= (unitVect[i].crossProduct(unitVect2[i]))/(unitVect[i].crossProduct(unitVect2[i])).magnitude();
-    else
-      n1 = 0.0;
-      nn =unitVect[i].crossProduct(unitVect2[i]).magnitude();
-    if((unitVect2[i].crossProduct(unitVect[i])).magnitude() != 0)
-      n2 =  (unitVect2[i].crossProduct(unitVect[i]))/(unitVect2[i].crossProduct(unitVect[i])).magnitude();
-    else
-      n2 = 0.0;
-    if(n1.dotProduct(n2)>1)
-      temp1 = 1.0;
-    else
-      temp1 = n1.dotProduct(n2);
-    sign =(n1.crossProduct(n2)).dotProduct(unitVect[i]); 
-    sum += nn; 
+    
+     area = unitVect[i].crossProduct(unitVect2[i]).magnitude();
+  
+    sign =(unitVect2[i].dotProduct(unitVect[i]));
+    sum += area; 
   }
- sign2 = sign/fabs(sign);
-  return  sum/(4.0); 
+    sign2 = sign/fabs(sign);
+  return  sum/(4.0) * sign2; 
 }
 
 void KnotTheoryDistance::PrintOptions(ostream& _os) const {
