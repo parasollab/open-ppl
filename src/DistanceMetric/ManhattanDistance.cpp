54fa0fe20df60a7a8ddf82bd2886ae711e8e02b1
Shawna Thomas <sthomas@cse.tamu.edu>
2012-05-17 19:59:55 +0000

    bug fix, c2 never considered in distance calculation
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@3263 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/DistanceMetric/ManhattanDistance.cpp
+++ src/DistanceMetric/ManhattanDistance.cpp
@@ -21,6 +21,7 @@ void ManhattanDistance::PrintOptions(ostream& _os) const {
 double ManhattanDistance::Distance(Environment* _env, const Cfg& _c1, const Cfg& _c2) {
   double dist = 0;
   Cfg *pC = _c1.CreateNewCfg();
+  pC->subtract(_c1,_c2);
   vector<double> dt = pC->GetData(); // position values
   for(size_t i=0; i < dt.size(); i++) {
     if(dt[i] < 0) 
