d9f87fcd6399768254f26c8d9fb6c14b6c8044e8
Jory Denny <jdenny@cse.tamu.edu>
2015-01-06 21:28:00 +0000

    Fix to approx reduce noise function. Has issues with fixed based linkage midpoint computations. For now, just doing an extra boundary check.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/branches/MAJournal@4494 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/LocalPlanners/MedialAxisLP.h
+++ src/LocalPlanners/MedialAxisLP.h
@@ -754,6 +754,7 @@ ReduceNoise(const CfgType& _c1, const CfgType& _c2,
   //  else
   //    add old intermediate
 
+  Environment* env = this->GetMPProblem()->GetEnvironment();
   LPOutput<MPTraits> lpOutput;
 
   CfgType col;
@@ -767,7 +768,9 @@ ReduceNoise(const CfgType& _c1, const CfgType& _c2,
 
     CfgType mid = (*cit1 + *cit2)/2;
 
-    if(m_envLP.IsConnected(prevMid, mid, col, &lpOutput, _posRes, _oriRes, true, true, true)) {
+    if(env->InBounds(prevMid, env->GetBoundary()) &&
+        env->InBounds(mid, env->GetBoundary()) &&
+        m_envLP.IsConnected(prevMid, mid, col, &lpOutput, _posRes, _oriRes, true, true, true)) {
       if(newIntermediates.empty() || newIntermediates.back() != prevMid)
         newIntermediates.push_back(prevMid);
       newIntermediates.push_back(mid);
@@ -781,7 +784,9 @@ ReduceNoise(const CfgType& _c1, const CfgType& _c2,
   //check last goal
   CfgType mid = (_c2 + _lpOutput->m_intermediates.back())/2;
 
-  if(m_envLP.IsConnected(prevMid, mid, col, &lpOutput, _posRes, _oriRes, true, true, true)) {
+  if(env->InBounds(prevMid, env->GetBoundary()) &&
+      env->InBounds(mid, env->GetBoundary()) &&
+      m_envLP.IsConnected(prevMid, mid, col, &lpOutput, _posRes, _oriRes, true, true, true)) {
     if(newIntermediates.empty() || newIntermediates.back() != prevMid)
       newIntermediates.push_back(prevMid);
     newIntermediates.push_back(mid);
