feca9106e460c33efa250a3e7931ceab9183efc0
Jory Denny <jdenny@cse.tamu.edu>
2015-03-02 19:34:33 +0000

    Adding nightly collection of parallel compile.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/trunk@4512 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- validation/pmpl_nightly_src_mail.pl
+++ validation/pmpl_nightly_src_mail.pl
@@ -61,9 +61,12 @@ $errcode = max($errcode,
 $errcode = max($errcode,
   &get_error_code("$outputdir/$fulldate/pmpl.LINUX_gcc.multi.debug1.parallel0.out",
     "Configuration: gcc 4.8.2 (platform=LINUX_gcc, robot=multi, debug=1, parallel=0)"));
-#$errcode = max($errcode,
-#  &get_error_code("$outputdir/$fulldate/pmpl.LINUX_gcc.reach.debug0.parallel0.out",
-#    "Configuration: gcc 4.8.2 (platform=LINUX_gcc, robot=reach, debug=0, parallel=0)"));
+$errcode = max($errcode,
+  &get_error_code("$outputdir/$fulldate/pmpl.LINUX_gcc.cfg.debug0.parallel1.out",
+    "Configuration: gcc 4.8.2 (platform=LINUX_gcc, robot=cfg, debug=0, parallel=1)"));
+$errcode = max($errcode,
+  &get_error_code("$outputdir/$fulldate/pmpl.LINUX_gcc.cfg.debug1.parallel1.out",
+    "Configuration: gcc 4.8.2 (platform=LINUX_gcc, robot=cfg, debug=1, parallel=1)"));
 
 #
 # Send out e-mail
