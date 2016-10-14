a053e48af140b911983b7c64f7a944f3fbfbacd8
Read Sandstrom <readamus@tamu.edu>
2016-10-14 22:00:33 +0000

    More progress on subroutines.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/branches/Syclop@5017 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/MPStrategies/Syclop.h
+++ src/MPStrategies/Syclop.h
@@ -3,6 +3,7 @@
 
 #include <iomanip>
 #include <map>
+#include <vector>
 
 #include "BasicRRTStrategy.h"
 
@@ -70,13 +71,15 @@ class Syclop : public BasicRRTStrategy<MPTraits> {
     ///@{
 
     virtual VID Extend(const VID _nearVID, const CfgType& _qRand,
-        const bool _lp = false);
+        const bool _lp = false) override;
+
+    virtual pair<VID, bool> AddNode(const CfgType& _newCfg) override;
 
     ///@}
     ///\name Tree Helpers
     ///@{
 
-    virtual VID ExpandTree(const VID _nearestVID, const CfgType& _target);
+    virtual VID ExpandTree(const VID _nearestVID, const CfgType& _target) override;
 
     ///@}
     ///@name Syclop Functions
@@ -103,6 +106,7 @@ class Syclop : public BasicRRTStrategy<MPTraits> {
     ////////////////////////////////////////////////////////////////////////////
     /// @brief Find the workspace region that holds a given configuration.
     RegionPointer LocateRegion(const VID _v);
+    RegionPointer LocateRegion(const Point3d& _p);
 
     ////////////////////////////////////////////////////////////////////////////
     /// @brief Update the coverage of the region containing a new vertex.
@@ -153,15 +157,37 @@ class Syclop : public BasicRRTStrategy<MPTraits> {
       double weight; ///< Relative probability of selecting this region from a lead.
       double alpha;  ///< The edge-weight coefficient for this region.
 
+      double freeVolume; ///< The estimated free state-space volume of this region.
+
+      size_t numTimesSelected{0}; ///< The number of times this region has been selected.
+
+      vector<VID> vertices; ///< The VID's of the configurations in this region.
+
+      void UpdateAlpha(RegionPointer _r) {
+        alpha = 1. / ((1. + Cov(_r)) * pow(FreeVol(_r), 4));
+      }
+
+      void UpdateWeight(RegionPointer _r) {
+        weight = pow(FreeVol(_r), 4) /
+            ((1. + Cov(_r)) * (1. + pow(numTimesSelected, 2)));
+      }
+
       ///@}
 
     };
 
+    /// Holds extra data associated with the regions.
     map<RegionPointer, RegionData> m_regionData;
 
     // Some kind of coverage map/structure
 
     // Some kind of connectivity map/structure
+
+    ///@}
+    ///@name Pre-processing Stuff
+    ///@{
+
+    void ComputeFreeVolumes();
 
     ///@}
 };
@@ -189,6 +215,10 @@ void
 Syclop<MPTraits>::
 Initialize() {
   BasicRRTStrategy<MPTraits>::Initialize();
+
+  // TODO: Decompose workspace.
+
+  ComputeFreeVolumes();
 }
 
 
@@ -225,8 +255,32 @@ template<class MPTraits>
 typename Syclop<MPTraits>::VID
 Syclop<MPTraits>::
 Extend(const VID _nearVID, const CfgType& _qRand, const bool _lp) {
+  // TODO: if we extended into a region that wasn't previously available, add it
+  // to the list of available regions.
   return BasicRRTStrategy<MPTraits>::Extend(_nearVID, _qRand, _lp);
 }
+
+
+template<class MPTraits>
+pair<typename BasicRRTStrategy<MPTraits>::VID, bool>
+Syclop<MPTraits>::
+AddNode(const CfgType& _newCfg) {
+  auto added = BasicRRTStrategy<MPTraits>::AddNode(_newCfg);
+
+  // If node is new and not invalid, update region data.
+  if(added.second) {
+    // Add this VID to the appropriate region.
+    RegionPointer r = LocateRegion(added.first);
+    m_regionData[r].vertices.push_back(added.first);
+
+    // Update region data.
+    m_regionData[r].UpdateAlpha(r);
+    m_regionData[r].UpdateWeight(r);
+  }
+
+  return added;
+}
+
 
 /*------------------------------ Tree Helpers --------------------------------*/
 
@@ -244,10 +298,21 @@ template <typename MPTraits>
 vector<typename Syclop<MPTraits>::RegionPointer>
 Syclop<MPTraits>::
 DiscreteLead() {
-  // Apply weights to region graph edges.
+  static constexpr double probabilityOfDijkstras = .95;
+  // TODO: Apply weights to region graph edges.
+
   // Search region graph from start to goal.
+  vector<RegionPointer> path;
+
+  if(DRand() < probabilityOfDijkstras) {
+    // TODO: Search with djikstra's.
+  }
+  else {
+    // TODO: Search with DFS, random child ordering.
+  }
+
   // Return path.
-  return vector<RegionPointer>();
+  return path;
 }
 
 
@@ -255,7 +320,10 @@ template <typename MPTraits>
 vector<typename Syclop<MPTraits>::RegionPointer>
 Syclop<MPTraits>::
 AvailableRegions(vector<RegionPointer> _lead) {
-  return vector<RegionPointer>();
+  vector<RegionPointer> available;
+  // TODO: select the available regions from the lead.
+
+  return available;
 }
 
 
@@ -263,6 +331,27 @@ template <typename MPTraits>
 typename Syclop<MPTraits>::RegionPointer
 Syclop<MPTraits>::
 SelectRegion(vector<RegionPointer> _available) {
+  // Compute the total weight of the available regions.
+  double totalWeight = 0.;
+  for(const auto& region : _available)
+    totalWeight += m_regionData[region].weight;
+
+  // Roll a die in range [0, 1].
+  const double roll = DRand();
+
+  // Choose a region based on relative weight.
+  double cumulative = 0.;
+  for(const auto& region : _available) {
+    cumulative += m_regionData[region].weight / totalWeight;
+    if(cumulative > roll) {
+      // Select this region!
+      ++m_regionData[region].numTimesSelected;
+      m_regionData[region].UpdateWeight(region);
+      return region;
+    }
+  }
+
+  throw RunTimeException(WHERE, "Failed to select a region by weight!");
   return nullptr;
 }
 
@@ -271,7 +360,14 @@ template <typename MPTraits>
 typename Syclop<MPTraits>::VID
 Syclop<MPTraits>::
 SelectVertex(RegionPointer _r) {
-  return INVALID_VID;
+  const auto& vertices = m_regionData[_r].vertices;
+
+  if(vertices.empty())
+    throw RunTimeException(WHERE, "Tried to select a vertex from a region with "
+        "no vertices.");
+
+  size_t randomIndex = Rand() % vertices.size();
+  return vertices[randomIndex];
 }
 
 
@@ -282,11 +378,26 @@ LocateRegion(const VID _v) {
   // Get the reference point for this configuration.
   auto point = this->GetRoadmap()->GetGraph()->find_vertex(_v)->property().
       GetPoint();
+  return LocateRegion(point);
+}
 
-  // Search region graph to see which region contains point.
-  auto regionGraph = this->GetEnvironment()->GetDecomposition();
-  for(const auto& r : *regionGraph) {
-    // TODO If point is inside r, return r.
+
+template <typename MPTraits>
+typename Syclop<MPTraits>::RegionPointer
+Syclop<MPTraits>::
+LocateRegion(const Point3d& _p) {
+  // Search region graph to see which region contains this point.
+  {
+    // Option 1: brute-force linear search
+    auto regionGraph = this->GetEnvironment()->GetDecomposition();
+    for(const auto& r : *regionGraph) {
+      // TODO If point is inside r, return r.
+    }
+  }
+  {
+    // Option 2: find the coverage grid cell that holds this vertex, then map the
+    // possible regions based on which ones touch that coverage cell.
+    // TODO
   }
 
   return nullptr;
@@ -297,6 +408,7 @@ template <typename MPTraits>
 void
 Syclop<MPTraits>::
 UpdateCoverage(const VID _v) {
+  // TODO
 }
 
 
@@ -304,6 +416,7 @@ template <typename MPTraits>
 void
 Syclop<MPTraits>::
 UpdateConnections(const VID _v1, const VID _v2) {
+  // TODO
 }
 
 /*------------------------------ Syclop Helpers ------------------------------*/
@@ -312,17 +425,20 @@ template <typename MPTraits>
 size_t
 Syclop<MPTraits>::
 Cov(RegionPointer _r) {
-  // Initially, just approximate the number of cells as number of vertices in
-  // the region.
-  return 0;
+  // Initially, just approximate the number of occupied cells as number of
+  // vertices in the region.
+  return m_regionData[_r].vertices.size();
+
+  // TODO use the number of occupied coverage grid cells instead.
 }
 
 
 template <typename MPTraits>
+inline
 double
 Syclop<MPTraits>::
 FreeVol(RegionPointer _r) {
-  return 0;
+  return m_regionData[_r].freeVolume;
 }
 
 
@@ -330,6 +446,7 @@ template <typename MPTraits>
 size_t
 Syclop<MPTraits>::
 Sel(RegionPointer _r1, RegionPointer _r2) {
+  // TODO
   return 0;
 }
 
@@ -338,6 +455,7 @@ template <typename MPTraits>
 size_t
 Syclop<MPTraits>::
 Conn(RegionPointer _r1, RegionPointer _r2) {
+  // TODO
   return 0;
 }
 
@@ -346,10 +464,59 @@ template <typename MPTraits>
 double
 Syclop<MPTraits>::
 Cost(RegionPointer _r1, RegionPointer _r2) {
-  static auto alpha = [](RegionPointer _r) {
-    return 1. / ((1. + Cov(_r)) * pow(FreeVol(_r), 4));
-  };
-  return alpha(_r1) * alpha(_r2) * (1. + pow(Sel(_r1, _r2), 2)) / (1. + pow(Conn(_r1, _r2), 2));
+  double& a1 = m_regionData[_r1].alpha;
+  double& a2 = m_regionData[_r2].alpha;
+  return a1 * a2 * (1. + pow(Sel(_r1, _r2), 2)) / (1. + pow(Conn(_r1, _r2), 2));
+}
+
+/*--------------------------- Pre-processing Stuff ---------------------------*/
+
+template <typename MPTraits>
+double
+Syclop<MPTraits>::
+ComputeFreeVolumes() {
+  static constexpr double eps = 1;
+  static constexpr size_t numSamples = 5000;
+
+  this->GetStatClass()->StartClock("ComputeFreeVolumes");
+
+  // Make a fixed number of samples.
+  auto sampler = this->GetSampler("UniformRandom");
+  auto boundary = this->GetEnvironment()->GetBoundary();
+
+  vector<CfgType> samples;
+  sampler->Sample(numSamples, 1, boundary, back_inserter(samples));
+
+  // Assert that we made the right number of samples.
+  if(samples.size() != numSamples)
+    throw RunTimeException(WHERE, "Tried to make " + to_string(numSamples) +
+        " samples, but instead produced " + to_string(samples.size()));
+
+  // Count the number of valid and invalid samples in each region.
+  auto vc = this->GetValidityChecker("pqp_solid");
+
+  map<RegionPointer, pair<size_t, size_t>> results;
+  for(const auto& sample : samples) {
+    RegionPointer r = LocateRegion(sample.GetPoint());
+    if(vc->IsValid(samples[i], "Syclop::ComputeFreeVolumes"))
+      ++results[r].first;
+    else
+      ++results[r].second;
+  }
+
+  // For each region, compute the approximate free volume using the samples.
+  for(auto& regionData : m_regionData) {
+    auto& region = regionData.first;
+    auto& data = regionData.second;
+
+    const size_t numValid = results[region].first;
+    const size_t numInvalid = results[region].second;
+
+    data.freeVolume = (eps + numValid) / (eps + numValid + numInvalid);
+    data.freeVolume *= 1./* TODO: replace with volume of region */;
+  }
+
+  this->GetStatClass()->StopClock("ComputeFreeVolumes");
 }
 
 /*----------------------------------------------------------------------------*/
