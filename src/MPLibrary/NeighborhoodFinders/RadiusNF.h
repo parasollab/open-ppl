6e70e0f66f9cfabaf7dacdd67f9df0faa16680b6
Read Sandstrom <readamus@tamu.edu>
2017-08-24 23:23:30 +0000

    Fixing bug in radius NF reported by Jory, where only one Cfg at a given distance would be reported. This was caused by storing the neighbors in a set instead of multiset. Also updating the class to standards.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/branches/simulator-refactoring@5492 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/MPLibrary/NeighborhoodFinders/RadiusNF.h
+++ src/MPLibrary/NeighborhoodFinders/RadiusNF.h
@@ -3,60 +3,114 @@
 
 #include "NeighborhoodFinderMethod.h"
 
+
 ////////////////////////////////////////////////////////////////////////////////
+/// Find all roadmap nodes within a given radius of a query configuration.
 /// @ingroup NeighborhoodFinders
-/// @brief Find neighbors within a certain radius.
 ////////////////////////////////////////////////////////////////////////////////
 template <typename MPTraits>
 class RadiusNF: public NeighborhoodFinderMethod<MPTraits> {
 
   public:
 
+    ///@name Motion Planning Types
+    ///@{
+
     typedef typename MPTraits::CfgType      CfgType;
     typedef typename MPTraits::RoadmapType  RoadmapType;
     typedef typename RoadmapType::VID       VID;
     typedef typename RoadmapType::GraphType GraphType;
 
-    RadiusNF(string _dmLabel = "", bool _unconnected = false, double _r = 1.0,
-        bool _useFallback = false) :
-      NeighborhoodFinderMethod<MPTraits>(_dmLabel, _unconnected) {
-        this->SetName("RadiusNF");
-        this->m_nfType = RADIUS;
-        this->m_radius = _r;
-        m_useFallback = _useFallback;
-      }
+    ///@}
+    ///@name Construction
+    ///@{
 
-    RadiusNF(XMLNode& _node) :
-      NeighborhoodFinderMethod<MPTraits>(_node) {
-        this->SetName("RadiusNF");
-        this->m_nfType = RADIUS;
-        this->m_radius = _node.Read("radius", true, 0.5, 0.0, MAX_DBL, "Radius");
-        m_useFallback = _node.Read("useFallback", false, false,
-            "Use nearest node if none are found within the radius.");
-      }
+    /// Construct a radius neighborhood finder.
+    /// @param _dmLabel The distance metric to use.
+    /// @param _unconnected Return only neighbors that are not connected?
+    /// @param _r Return all nodes within this distance of the query point.
+    /// @param _useFallback Return the nearest node if none are in the radius?
+    RadiusNF(const std::string& _dmLabel = "", const bool _unconnected = false,
+        const double _r = 1.0, const bool _useFallback = false);
 
-    virtual void Print(ostream& _os) const {
-      NeighborhoodFinderMethod<MPTraits>::Print(_os);
-      _os << "\tradius: " << this->m_radius << endl;
-      _os << "\tuse nearest: " << m_useFallback << endl;
-    }
+    /// Construct a radius neighborhood finder from an XML node.
+    /// @param _node The XML node.
+    RadiusNF(XMLNode& _node);
+
+    virtual ~RadiusNF() = default;
+
+    ///@}
+    ///@name MPBaseObject Overrides
+    ///@{
+
+    virtual void Print(ostream& _os) const override;
+
+    ///@}
+    ///@name NeighborhoodFinderMethod Interface
+    ///@{
 
     template<typename InputIterator, typename OutputIterator>
-      OutputIterator FindNeighbors(RoadmapType* _rmp,
-          InputIterator _first, InputIterator _last, bool _fromFullRoadmap,
-          const CfgType& _cfg, OutputIterator _out);
+    OutputIterator FindNeighbors(RoadmapType* _rmp,
+        InputIterator _first, InputIterator _last, bool _fromFullRoadmap,
+        const CfgType& _cfg, OutputIterator _out);
 
     template<typename InputIterator, typename OutputIterator>
-      OutputIterator FindNeighborPairs(RoadmapType* _rmp,
-          InputIterator _first1, InputIterator _last1,
-          InputIterator _first2, InputIterator _last2,
-          OutputIterator _out);
+    OutputIterator FindNeighborPairs(RoadmapType* _rmp,
+        InputIterator _first1, InputIterator _last1,
+        InputIterator _first2, InputIterator _last2,
+        OutputIterator _out);
+
+    ///@}
 
   private:
 
+    ///@name Internal State
+    ///@{
+
     bool m_useFallback{false}; ///< Use the nearest node if no nodes are found
                                ///< within the radius.
+
+    ///@}
+
 };
+
+/*------------------------------ Construction --------------------------------*/
+
+template <typename MPTraits>
+RadiusNF<MPTraits>::
+RadiusNF(const std::string& _dmLabel, const bool _unconnected,
+    const double _r, const bool _useFallback) :
+    NeighborhoodFinderMethod<MPTraits>(_dmLabel, _unconnected) {
+  this->SetName("RadiusNF");
+  this->m_nfType = RADIUS;
+  this->m_radius = _r;
+  m_useFallback = _useFallback;
+}
+
+
+template <typename MPTraits>
+RadiusNF<MPTraits>::
+RadiusNF(XMLNode& _node) : NeighborhoodFinderMethod<MPTraits>(_node) {
+  this->SetName("RadiusNF");
+  this->m_nfType = RADIUS;
+  this->m_radius = _node.Read("radius", true, 0.5, 0.0, MAX_DBL, "Radius");
+  m_useFallback = _node.Read("useFallback", false, false,
+      "Use nearest node if none are found within the radius.");
+}
+
+/*-------------------------- MPBaseObject Overrides --------------------------*/
+
+template <typename MPTraits>
+void
+RadiusNF<MPTraits>::
+Print(std::ostream& _os) const {
+  NeighborhoodFinderMethod<MPTraits>::Print(_os);
+  _os << "\tradius: " << this->m_radius
+      << "\n\tuse nearest: " << m_useFallback
+      << std::endl;
+}
+
+/*------------------- NeighborhoodFinderMethod Functions ---------------------*/
 
 template <typename MPTraits>
 template<typename InputIterator, typename OutputIterator>
@@ -72,7 +126,7 @@ FindNeighbors(RoadmapType* _rmp,
 
   GraphType* map = _rmp->GetGraph();
   auto dmm = this->GetDistanceMetric(this->m_dmLabel);
-  set<pair<VID, double>, CompareSecond<VID, double> > inRadius;
+  std::multiset<pair<VID, double>, CompareSecond<VID, double> > inRadius;
 
   // The node used as a fallback if m_useFallback is set. Pair of VID and
   // distance from _cfg. This is returned if no nodes are found within the
@@ -114,6 +168,7 @@ FindNeighbors(RoadmapType* _rmp,
   return copy(inRadius.begin(), inRadius.end(), _out);
 }
 
+
 template <typename MPTraits>
 template<typename InputIterator, typename OutputIterator>
 OutputIterator
@@ -124,7 +179,7 @@ RadiusNF<MPTraits>::FindNeighborPairs(RoadmapType* _rmp,
 
   GraphType* map = _rmp->GetGraph();
   auto dmm = this->GetDistanceMetric(this->m_dmLabel);
-  set<pair<pair<VID, VID >, double> > inRadius;
+  std::multiset<pair<pair<VID, VID >, double> > inRadius;
 
   // Find all pairs within radius
   for(InputIterator it1 = _first1; it1 != _last1; it1++) {
@@ -146,5 +201,7 @@ RadiusNF<MPTraits>::FindNeighborPairs(RoadmapType* _rmp,
 
   return copy(inRadius.begin(), inRadius.end(), _out);
 }
+
+/*----------------------------------------------------------------------------*/
 
 #endif
