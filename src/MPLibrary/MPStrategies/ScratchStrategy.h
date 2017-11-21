59afc65cb79a2d7dbdee998e4d2aa5ee097fb674
Read Sandstrom <readamus@tamu.edu>
2017-11-21 17:36:40 +0000

    Adding missed file.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/branches/simulator-refactoring@5757 5e2c2758-810a-0410-8eb3-8e4e92d4004e

---
+++ src/MPLibrary/MPStrategies/ScratchStrategy.h
@@ -0,0 +1,83 @@
+#ifndef SCRATCH_STRATEGY_H_
+#define SCRATCH_STRATEGY_H_
+
+#include "MPStrategyMethod.h"
+#include "nonstd/io.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Scratch space for testing random stuff in PMPL.
+////////////////////////////////////////////////////////////////////////////////
+template <typename MPTraits>
+class ScratchStrategy : public MPStrategyMethod<MPTraits> {
+
+  public:
+
+    ///@name Motion Planning Types
+    ///@{
+
+    typedef typename MPTraits::CfgType      CfgType;
+    typedef typename MPTraits::WeightType   WeightType;
+    typedef typename MPTraits::RoadmapType  RoadmapType;
+    typedef typename RoadmapType::GraphType GraphType;
+    typedef typename RoadmapType::VID       VID;
+
+    ///@}
+    ///@name Construction
+    ///@{
+
+    ScratchStrategy();
+
+    ScratchStrategy(XMLNode& _node);
+
+    virtual ~ScratchStrategy() = default;
+
+    ///@}
+    ///@name MPStrategyMethod Overrides
+    ///@{
+
+    virtual void Iterate() override;
+    virtual void Finalize() override;
+
+    ///@}
+
+};
+
+/*----------------------------- Construction ---------------------------------*/
+
+template <typename MPTraits>
+ScratchStrategy<MPTraits>::
+ScratchStrategy() : MPStrategyMethod<MPTraits>() {
+  this->SetName("ScratchStrategy");
+}
+
+
+template <typename MPTraits>
+ScratchStrategy<MPTraits>::
+ScratchStrategy(XMLNode& _node) : MPStrategyMethod<MPTraits>(_node) {
+  this->SetName("ScratchStrategy");
+}
+
+/*------------------------------ Interface -----------------------------------*/
+
+template <typename MPTraits>
+void
+ScratchStrategy<MPTraits>::
+Iterate() {
+  MethodTimer mt(this->GetStatClass(), "Test Iteration");
+}
+
+
+template <typename MPTraits>
+void
+ScratchStrategy<MPTraits>::
+Finalize() {
+  // Output stats.
+  ofstream  osStat(this->GetBaseFilename() + ".stat");
+  StatClass* stats = this->GetStatClass();
+  stats->PrintAllStats(osStat, this->GetRoadmap());
+}
+
+/*----------------------------------------------------------------------------*/
+
+#endif
