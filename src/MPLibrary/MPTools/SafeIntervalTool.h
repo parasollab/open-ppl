6f03aef8b4b632e197611f1aa578b01a4b2c0787
James Motes <jmotes@tamu.edu>
2018-01-24 04:37:09 +0000

    Added SafeIntervalTool::ComputeSafeInterval functionality
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/branches/simulator-refactoring@5795 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/MPLibrary/MPTools/SafeIntervalTool.h
+++ src/MPLibrary/MPTools/SafeIntervalTool.h
@@ -12,7 +12,7 @@
 /// of time where collision with a known dynamic obstacle (with known
 /// trajectory) will not occur.
 ///
-/// This tool implements the concept of a 'Safe Interval' from the paper:
+/// This tool implements the concept of a 'Safe Intervals' from the paper:
 ///
 /// Phillips, Mike, and Maxim Likhachev. "Sipp: Safe interval path planning for
 /// dynamic environments." Robotics and Automation (ICRA), 2011 IEEE International
@@ -33,7 +33,7 @@ class SafeIntervalTool final : public MPBaseObject<MPTraits> {
     ///@name Local Types
     ///@{
 
-    typedef std::vector<Range<double>> Interval;
+    typedef std::vector<Range<double>> Intervals;
 
     ///@}
     ///@name Construction
@@ -46,18 +46,18 @@ class SafeIntervalTool final : public MPBaseObject<MPTraits> {
     virtual ~SafeIntervalTool();
 
     ///@}
-    ///@name Interval Computation
+    ///@name Intervals Computation
     ///@{
 
     /// Compute the safe intervals for a given Cfg.
-    /// @param _cfg The configuration to compute SI's for.
+    /// @param _cfg The configuration to compute safeIntervals's for.
     /// @return The set of safe intervals for _cfg.
-    Interval ComputeInterval(const CfgType& _cfg);
+    Intervals ComputeIntervals(const CfgType& _cfg);
 
     /// Compute the safe intervals for a given Edge.
-    /// @param _weight The edge to compute SI's for.
+    /// @param _weight The edge to compute safeIntervals's for.
     /// @return The set of safe intervals for _weight.
-    Interval ComputeInterval(const WeightType& _weight);
+    Intervals ComputeIntervals(const WeightType& _weight);
 
     ///@}
 
@@ -71,6 +71,11 @@ class SafeIntervalTool final : public MPBaseObject<MPTraits> {
     /// @param _timestep The timestep for the dynamic obstacles.
     bool IsSafe(const CfgType& _cfg, const double _timestep);
 
+    /// Computes the safe interval(s) for a Cfg or Edge
+    /// @param _cfgs The cfg(s) to compute the safeIntervalss for.
+    /// Represent a cfg as a vector of size 1 and an 
+    /// edge as a vector of cfgs. 
+    Intervals ComputeSafeIntervals(const std::vector<Cfg>& _cfgs);
     ///@}
 
     ///@name Internal State
@@ -101,23 +106,23 @@ template <typename MPTraits>
 SafeIntervalTool<MPTraits>::
 ~SafeIntervalTool() = default;
 
-/*-------------------------- Interval Computations ---------------------------*/
+/*-------------------------- Intervals Computations ---------------------------*/
 
 template <typename MPTraits>
-typename SafeIntervalTool<MPTraits>::Interval
+typename SafeIntervalTool<MPTraits>::Intervals
 SafeIntervalTool<MPTraits>::
-ComputeInterval(const CfgType& _cfg) {
-
-  return Interval();
+ComputeIntervals(const CfgType& _cfg) {
+  std::vector<Cfg> cfg = {_cfg};
+  return ComputeSafeIntervals(&cfg);
 }
 
 
 template <typename MPTraits>
-typename SafeIntervalTool<MPTraits>::Interval
+typename SafeIntervalTool<MPTraits>::Intervals
 SafeIntervalTool<MPTraits>::
-ComputeInterval(const WeightType& _weight) {
-  //vector<Cfg>& cfgs = _weight->GetIntermediates()
-  return Interval();
+ComputeIntervals(const WeightType& _weight) {
+  vector<Cfg>& cfgs = _weight->GetIntermediates();
+  return ComputeSafeIntervals(&cfgs);
 }
 
 /*--------------------------------- Helpers ----------------------------------*/
@@ -134,15 +139,20 @@ IsSafe(const CfgType& _cfg, const double _timestep) {
   const double timeRes = env->GetTimeRes();
 
   const long currentStep = std::lround(_timestep / timeRes);
-
+  
+  Cfg* currentObstaclePosition;
+  
   for(auto& obstacle : obstacles){
-    if (_timestep > (timeRes * obstacle->m_path.size()))
-      continue;
-
+    if (_timestep > (timeRes * obstacle->m_path.size())){
+      currentObstaclePosition = obstacle->m_path.back();
+    } 
+    else{
+      currentObstaclePosition = obstacle->m_path[currentStep];
+    }
     // If the obstacle is in collision with _cfg at _timestep, return false
     CDInfo cdinfo;
     if(vc->IsInCollision(cdinfo,
-        obstacle->m_path[currentStep].GetRobot()->GetMultiBody(),
+        currentObstaclePosition->GetRobot()->GetMultiBody(),
         _cfg.GetRobot()->GetMultiBody(), this->GetNameAndLabel())) {
       return false;
     }
@@ -150,15 +160,60 @@ IsSafe(const CfgType& _cfg, const double _timestep) {
   return true;
 }
 
-// TODO: Implement helper function for ComputeInterval
+// TODO: Implement helper function for ComputeIntervals
 // TODO: rename
-/*template <typename MPTraits>
-typename SafeIntervalTool<MPTraits>::Interval
+template <typename MPTraits>
+typename SafeIntervalTool<MPTraits>::Intervals
 SafeIntervalTool<MPTraits>::
-ComputeInterval(std::vector<Cfg> _cfgs){
+ComputeSafeIntervals(const std::vector<Cfg>& _cfgs){
+  MethodTimer mt(this->GetStatClass(), "SafeIntervalTool::ComputeSafeIntervals");
+  Intervals safeIntervals;
+  Range<double>* currentInterval = nullptr;
+  
+  // Find how long there is potential for collision with dynamic obstacles
+  // following known paths.
+  // Need a different distinction to account for period motion of DOs.
+  size_t timeFinal = 0;
+  const auto& obstacles = this->GetMPProblem()->GetDynamicObstacles();
+  for(auto& obstacle : obstacles){
+    if(obstacle->m_path.size() > timeFinal){
+      timeFinal = obstacle->m_path.size();
+    }
+  }
 
-  return Interval();
-}*/
+
+  auto env = this->GetEnvironment();
+  const double timeRes = env->GetTimeRes();
+
+  for(size_t tstep = 0; tstep < timeFinal; tstep++){
+    bool safe = true;
+    int i = 0; // iterates over an edge to find safe interval 
+               //to start down an edge
+    for(Cfg cfg : _cfgs){
+      if(IsSafe(cfg,(tstep+i)*timeRes)){
+        i++;
+        continue;
+      }
+      safe = false;
+      break;
+    }
+    // still safe and expanding current interval
+    if(safe and currentInterval){
+      currentInterval->max = tstep*timeRes;
+    }
+    // no longer safe and ending current interval
+    else if(!safe and currentInterval){
+      safeIntervals.push_back(*currentInterval);
+      delete currentInterval;
+      currentInterval = nullptr;
+    }
+    // newly safe so starting new interval
+    else if(safe and !currentInterval){
+      currentInterval = new Range<double>(tstep*timeRes,tstep*timeRes);
+    }
+  }
+  return safeIntervals;
+}
 /*----------------------------------------------------------------------------*/
 
 #endif
