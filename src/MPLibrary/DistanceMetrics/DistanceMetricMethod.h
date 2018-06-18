2dffb6c05385ec4f2eea6ad945064582529e8ee9
Tobi Ogunyale <togunyale1@student.gsu.edu>
2018-06-18 20:31:57 +0000

    Added EdgeWeight function to Distance Metric
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/branches/simulator-refactoring@5910 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/MPLibrary/DistanceMetrics/DistanceMetricMethod.h
+++ src/MPLibrary/DistanceMetrics/DistanceMetricMethod.h
@@ -46,27 +46,33 @@ class DistanceMetricMethod  : public MPBaseObject<MPTraits> {
     DistanceMetricMethod(XMLNode& _node);
     virtual ~DistanceMetricMethod() = default;
 
-    ///@}
-    ///@name Distance Interface
-    ///@{
+      ///@}
+      ///@name Distance Interface
+      ///@{
 
-    /// @brief Compute a distance between two configurations
-    /// @param _c1 Configuration 1
-    /// @param _c2 Configuration 2
-    /// @return Distance value
+      /// @brief Compute a distance between two configurations
+      /// @param _c1 Configuration 1
+      /// @param _c2 Configuration 2
+      /// @return Distance value
     virtual double Distance(const CfgType& _c1, const CfgType& _c2) = 0;
 
-    /// @brief Scale a directional configuration to a certain magnitude
-    /// @param _length Desired magnitude
-    /// @param _c Configuration to be scaled
-    /// @param _o Configuration to scale upon (origin of scaling)
-    /// @return Distance value
+      /// @brief Scale a directional configuration to a certain magnitude
+      /// @param _length Desired magnitude
+      /// @param _c Configuration to be scaled
+      /// @param _o Configuration to scale upon (origin of scaling)
+      /// @return Distance value
     virtual void ScaleCfg(double _length, CfgType& _c,
-        const CfgType& _o);
+      const CfgType& _o);
+
+      /// @brief Compute the weight of an existing roadmap edge
+      /// @param _source VID source/start
+      /// @param _target VID target/end 
+      /// @return Edge Weight value 
+    virtual double EdgeWeight(const VID _source, const VID _target);
 
     void ScaleCfg(double _length, CfgType& _c);
 
-    ///@}
+      ///@}
 };
 
 /*------------------------------- Construction -------------------------------*/
@@ -82,14 +88,14 @@ template <typename MPTraits>
 void
 DistanceMetricMethod<MPTraits>::
 ScaleCfg(double _length, CfgType& _c, const CfgType& _o) {
-  _length = fabs(_length); //a distance must be positive
-  CfgType origin = _o;
-  CfgType outsideCfg = _c;
-  // first find an outsite configuration with sufficient size
-  while(Distance(origin, outsideCfg) < 2*_length)
-    for(size_t i=0; i<outsideCfg.DOF(); ++i)
-      outsideCfg[i] *= 2.0;
-  // now, using binary search find a configuration with the approximate length
+_length = fabs(_length); //a distance must be positive
+CfgType origin = _o;
+CfgType outsideCfg = _c;
+// first find an outsite configuration with sufficient size
+while(Distance(origin, outsideCfg) < 2*_length)
+  for(size_t i=0; i<outsideCfg.DOF(); ++i)
+    outsideCfg[i] *= 2.0;
+// now, using binary search find a configuration with the approximate length
   CfgType aboveCfg = outsideCfg;
   CfgType belowCfg = origin;
   CfgType currentCfg = _c;
@@ -114,6 +120,49 @@ void
 DistanceMetricMethod<MPTraits>::
 ScaleCfg(double _length, CfgType& _c) {
   ScaleCfg(_length, _c, CfgType(_c.GetRobot()));
+}
+
+
+template <typename MPTraits>
+double
+DistanceMetricMethod<MPTraits>::
+EdgeWeight(const VID _source, const VID _target) {
+  auto g = this->GetRoadmap()->GetGraph()
+  if( !( g->IsEdge(_source, _target) ) )
+
+    return std::numeric_limits<double>::infinity();
+
+  else {
+
+    auto edge = g->GetEdge( _source, _target);
+    
+    auto& intermediates = edges.GetIntermediates();
+
+    if( intermediates.empty()) {
+
+      return Distance(g->GetVertex(_source),g->GetVertex(_target) );
+
+    } else {
+
+      double intermediatesDist = 0.0;
+
+      intermediatesDist += Distance(g->GetVertex(_source),g->GetVertex(intermediates.front()));
+      intermediatesDist += Distance(g->GetVertex(intermediates.back(), g->GetVertex(_target)));
+
+      for(int iter = 0; iter < intermediates.size() - 1; iter ++) {
+        intermediatesDist += Distance(g->GetVertex(intermediates[iter]), g->GetVertex(intermediates[iter + 1]));
+      }
+
+      return intermediatesDist;
+
+
+
+    }
+
+
+
+  }
+
 }
 
 /*----------------------------------------------------------------------------*/
