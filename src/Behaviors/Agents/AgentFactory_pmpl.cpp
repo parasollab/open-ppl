07ac2c67416e9be8b142294a6fc81ac69da9a42a
Read Sandstrom <readamus@tamu.edu>
2018-11-08 23:26:57 +0000

    Adjusting Agent::Factory to ignore children of agent nodes when not running the simulator.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/pmpl/branches/simulator-refactoring@6022 5e2c2758-810a-0410-8eb3-8e4e92d4004e

--- src/Behaviors/Agents/AgentFactory_pmpl.cpp
+++ src/Behaviors/Agents/AgentFactory_pmpl.cpp
@@ -6,8 +6,21 @@
 std::unique_ptr<Agent>
 Agent::
 Factory(Robot* const _r, XMLNode& _node) {
-  // If we are not building the simulator, ignore the agent node.
+  // If we are not building the simulator, ignore the agent node and its
+  // children.
   _node.Ignore();
+
+  std::queue<XMLNode*> ignoredNodes;
+  ignoredNodes.push(&_node);
+
+  while(!ignoredNodes.empty()) {
+    XMLNode* node = ignoredNodes.front();
+    ignoredNodes.pop();
+
+    node->Ignore();
+    for(auto& child : *node)
+      ignoredNodes.push(&child);
+  }
 
   return {nullptr};
 }
