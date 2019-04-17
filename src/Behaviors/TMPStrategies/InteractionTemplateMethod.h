3ba31b5d8c7624d96babbb07a99a1afd32be4457
James Motes <jmotes@tamu.edu>
2019-04-17 14:06:04 -0500

    Updates to TMP template structure.

---
+++ src/Behaviors/TMPStrategies/InteractionTemplateMethod.h
@@ -0,0 +1,21 @@
+#ifndef INTERACTION_TEMPLATE_METHOD_H_
+#define INTERACTION_TEMPLATE_METHOD_H_
+
+#include "TMPStrategyMethod.h"
+
+/////////////////////////////////////////////////////////
+///
+///
+///
+////////////////////////////////////////////////////////
+
+class InteractionTemplateMethod {
+
+  public:
+
+
+  private:
+
+
+
+};
