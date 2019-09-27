84f989448b1efe4c41977fdb7047db0f236b33c0
Shawna Thomas <sthomas@cse.tamu.edu>
2019-09-27 15:04:52 -0500

    Changes to compile on MACOS_gcc (#6)
    * changes to compile with g++ on mac os
    
    * updated to compile vizmo and pmpl
    
    * changes to compile on MACOS_gcc for Qt an eigen libraries
    
    * setting default platform back to LINUX_gcc
    
    * reverting GLVIS changes, not needed
    
    * removing extra system printouts
    
    * passing along platform variable to other makefiles

--- src/Utilities/Color.h
+++ src/Utilities/Color.h
@@ -8,7 +8,11 @@ typedef mathtool::Vector<float, 4> Color4; ///< RGBA color, range from 0-1
 
 #ifdef VIZMO
 
-#include <gl.h>
+#ifdef __APPLE__
+  #include <OpenGL/gl.h>
+#else
+  #include <gl.h>
+#endif
 
 inline void SetGLColor(const Color3& _c) {glColor3fv(_c);} ///< Set color RGB
 inline void SetGLColor(const Color4& _c) {glColor4fv(_c);} ///< Set color RGBA
