74e5ae0317b054e05fe3490137705b83f300280d
readamus <readamus@tamu.edu>
2019-12-01 14:52:21 -0600

    Cleanups of old junk in the code and Metric classes. + Removed PushQueryToMA strategy because we no longer use 'queries'. + Removed CCDistance Metric because it doesn't represent a useful metric. The   code never uses the computed CCs and has multiple out-of-range errors. The   inteded purpose appears to have been measuring the maximum distance between   any two CCs, which isn't useful for planning. + Moved Connectivity and Coverage metrics to deadcode because these are   ill-formed. They do not represent any theoretically accepted definition of   connectivity or coverage, and rely on an external input file with oracle   information. + Moved Diameter Metric to deadcode because it isn't useful for planning. This   measures hops instead of edge weight or timesteps, and only considers the   largest CC.

--- src/Traits/CfgTraits.h
+++ src/Traits/CfgTraits.h
@@ -102,22 +102,14 @@
 
 //connector includes
 #include "MPLibrary/Connectors/AdaptiveConnector.h"
-//#include "MPLibrary/Connectors/CCExpansion.h"
 #include "MPLibrary/Connectors/CCsConnector.h"
 #include "MPLibrary/Connectors/NeighborhoodConnector.h"
 #include "MPLibrary/Connectors/RewireConnector.h"
 
 //metric includes
-#include "MPLibrary/Metrics/CCDistanceMetric.h"
-#include "MPLibrary/Metrics/ConnectivityMetric.h"
-#include "MPLibrary/Metrics/CoverageDistanceMetric.h"
-#include "MPLibrary/Metrics/CoverageMetric.h"
-#include "MPLibrary/Metrics/DiameterMetric.h"
 #include "MPLibrary/Metrics/NumEdgesMetric.h"
 #include "MPLibrary/Metrics/NumNodesMetric.h"
-#include "MPLibrary/Metrics/RoadmapSet.h"
 #include "MPLibrary/Metrics/TimeMetric.h"
-#include "MPLibrary/Metrics/VectorSet.h"
 
 //map evaluator includes
 #include "MPLibrary/MapEvaluators/ComposeEvaluator.h"
@@ -155,9 +147,7 @@
 #include "MPLibrary/MPStrategies/ModifyPath.h"
 #include "MPLibrary/MPStrategies/MultiStrategy.h"
 #include "MPLibrary/MPStrategies/PushCfgToMATest.h"
-#include "MPLibrary/MPStrategies/PushQueryToMA.h"
 #include "MPLibrary/MPStrategies/ScratchStrategy.h"
-//#include "MPLibrary/MPStrategies/SparkPRM.h"
 #include "MPLibrary/MPStrategies/StableSparseRRT.h"
 #include "MPLibrary/MPStrategies/Syclop.h"
 #include "MPLibrary/MPStrategies/TogglePRMStrategy.h"
@@ -303,37 +293,13 @@ struct MPTraits {
   //types of connectors available in our world
   typedef boost::mpl::list<
     AdaptiveConnector<MPTraits>,
-    //CCExpansion<MPTraits>,
     //CCsConnector<MPTraits>,
     NeighborhoodConnector<MPTraits>,
     RewireConnector<MPTraits>
       > ConnectorMethodList;
 
-  // RoadmapSet must be re-written before use.
-  //typedef ConnectivityMetric<MPTraits, RoadmapSet<MPTraits>>
-  //    ConnectivityMetricRoadmapSet;
-  //typedef CoverageDistanceMetric<MPTraits, RoadmapSet<MPTraits>>
-  //    CoverageDistanceMetricRoadmapSet;
-  //typedef CoverageMetric<MPTraits, RoadmapSet<MPTraits>>
-  //    CoverageMetricRoadmapSet;
-
-  typedef ConnectivityMetric<MPTraits, VectorSet<MPTraits>>
-      ConnectivityMetricVectorSet;
-  typedef CoverageDistanceMetric<MPTraits, VectorSet<MPTraits>>
-      CoverageDistanceMetricVectorSet;
-  typedef CoverageMetric<MPTraits, VectorSet<MPTraits>>
-      CoverageMetricVectorSet;
-
   //types of metrics available in our world
   typedef boost::mpl::list<
-    CCDistanceMetric<MPTraits>,
-    //ConnectivityMetricRoadmapSet,
-    //CoverageDistanceMetricRoadmapSet,
-    //CoverageMetricRoadmapSet,
-    ConnectivityMetricVectorSet,
-    CoverageDistanceMetricVectorSet,
-    CoverageMetricVectorSet,
-    DiameterMetric<MPTraits>,
     NumEdgesMetric<MPTraits>,
     NumNodesMetric<MPTraits>,
     TimeMetric<MPTraits>
@@ -379,10 +345,7 @@ struct MPTraits {
     GroupPRM<MPTraits>,
     ModifyPath<MPTraits>,
     MultiStrategy<MPTraits>,
-    PushQueryToMA<MPTraits>,
     ScratchStrategy<MPTraits>,
-//    SparkPRM<MPTraits, BasicPRM>,
-//    SparkPRM<MPTraits, TogglePRMStrategy>,
     StableSparseRRT<MPTraits>,
     Syclop<MPTraits>,
     TogglePRMStrategy<MPTraits>,
