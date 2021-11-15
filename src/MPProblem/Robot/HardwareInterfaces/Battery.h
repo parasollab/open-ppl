a8ae756520ec047c8cd41a60de59d919037ff178
martasm2 <martasm2@illinois.edu>
2021-11-14 23:17:25 -0600

    documentation checking/adding for MPProblem/Robot

--- src/MPProblem/Robot/HardwareInterfaces/Battery.h
+++ src/MPProblem/Robot/HardwareInterfaces/Battery.h
@@ -25,13 +25,34 @@ class Battery {
     ///@{
     /// @TODO Document these functions.
 
-    void SetValues(double, double);
+    /// Set max and current values of the battery
+    /// @param _max Max charge battery can hold
+    /// @param _cur Current charge
+    void SetValues(double _max, double _cur);
+
+    /// Set current level to max
     void SetToMax();
+
+    /// Print current and max battery levels
     void Print();
+
+    /// Get current battery level
     double GetCurLevel();
+
+    /// Get max charge battery can hold
     double GetMaxLevel();
+
+    /// Increase charge of battery, stop if over max
+    /// @param _increaseRate Amount to increase the current charge by
     void Charge(double _increaseRate);
+
+    /// Reduce value of charge by sum of base and moving rates
+    /// @param _depletionRateBase Base depletion rate of charge
+    /// @param _depletionRateMoving Depletion rate of charge while moving
     void UpdateValue(double _depletionRateBase, double _depletionRateMoving);
+
+    /// Reduce value of charge by base depletion rate
+    /// @param _depletionRateBase Base depletion rate of charge
     void UpdateValue(double _depletionRateBase);
 
     ///@}
