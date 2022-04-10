086be501d62dad5e7f506fc11dc0e0a0fd666e3e
merden2 <merden2@illinoi.edu>
2022-04-10 15:23:14 -0500

    Adding BruteForceNFTest test case

--- src/Testing/MPLibrary/NeighborhoodFinders/BruteForceNFTest.h
+++ src/Testing/MPLibrary/NeighborhoodFinders/BruteForceNFTest.h
@@ -14,6 +14,11 @@ class BruteForceNFTest : virtual public BruteForceNF<MPTraits>,
     ///@{
 
     typedef TestBaseObject::TestResult TestResult;
+    typedef typename MPTraits::RoadmapType RoadmapType;
+    typedef typename MPTraits::CfgType CfgType;
+    typedef typename MPTraits::WeightType WeightType;
+
+    typedef typename NeighborhoodFinderMethod<MPTraits>::OutputIterator OutputIterator;
 
     ///@}
     ///@name Construction
@@ -64,11 +69,76 @@ IndividualRobotFindNeighborsTest() {
   bool passed = true;
   std::string message = "";
 
-  if (passed)
-    message = "IndividualRobotFindNeighbors::PASSED!\n";
-  else
-    message = "IndividualRobotFindNeighbors::FAILED :(\n" + message;
-  return std::make_pair(passed,message);
+  //construct roadmap graph 
+
+  auto robot = NeighborhoodFinderMethodTest<MPTraits>::GetMPProblem()->GetRobots()[0].get();
+
+  RoadmapType* roadmap = new RoadmapType(robot);
+
+  // vertices 
+  auto p1 = CfgType(robot);
+  std::istringstream p1Stream("0 0 0 0 0 0 0");
+  p1.Read(p1Stream);
+  auto start = roadmap->AddVertex(p1);
+
+  auto p2 = CfgType(robot);
+  std::istringstream p2Stream("0 10 -10 0 0.5 3 0.9");
+  p2.Read(p2Stream);
+  auto goal = roadmap->AddVertex(p2);
+
+  auto v1P = CfgType(robot);
+  std::istringstream v1Stream("0 9 -4 0 0.3 0.7 0.1");
+  v1P.Read(v1Stream);
+  auto v1 = roadmap->AddVertex(v1P);
+
+  auto v2P = CfgType(robot);
+  std::istringstream v2Stream("0 20 0 0 0 0 0");
+  v2P.Read(v2Stream);
+  auto v2 = roadmap->AddVertex(v2P);
+
+  // Edges 
+  roadmap->AddEdge(start, goal, WeightType("a",1.0));
+  roadmap->AddEdge(start, v1, WeightType("b",2.0));
+  roadmap->AddEdge(start, v2, WeightType("c",3.0));
+  //roadmap->AddEdge(start, goal, WeightType("d",4.0));
+  //roadmap->AddEdge(start, goal, WeightType("e",5.0));
+
+  OutputIterator output; 
+
+  this->FindNeighbors(roadmap, start, nullptr, output); 
+
+  std::vector<size_t> vec;
+  vec.push_back(goal);
+  vec.push_back(v1);
+  vec.push_back(v2);
+  // vec = {goal, v1, v2};
+
+   if (this->m_k >= roadmap->Size()) {
+
+    if (output.size() == (roadmap->Size() -1)) {
+
+     passed = true; 
+     message = "IndividualRobotFindNeighbors::PASSED :(\n" + message;
+    }
+   }
+   else {
+
+    size_t distance_max = vec[this->m_k];
+
+    //outputs = neighbors that neighborhood finder finds closest to start
+
+    for (size_t i =0; i < output.size(); i++) {
+
+
+    //if the distance from base to vertex is less or equal distance max distance ?
+    if (output[i].distance > distance_max) {
+    passed = false; 
+    message = "IndividualRobotFindNeighbors::FAILED!\n";
+    }
+    }
+    }
+
+   return std::make_pair(passed,message);
 }
 
 template <typename MPTraits>    
