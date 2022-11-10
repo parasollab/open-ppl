66b6d0b035283b0af68185ac1a3cc7e2a0a28831
Ananya Yammanuru <ananyay2@illinois.edu>
2022-11-10 16:48:41 -0600

    Create PointConstruction class, sphere sampling fns

---
+++ src/MPLibrary/MPTools/PointConstruction.h
@@ -0,0 +1,129 @@
+#ifndef PMPL_POINT_CONSTRUCTION_H 
+#define PMPL_POINT_CONSTRUCTION_H
+
+#include <vector>
+#include "Vector.h"
+#include <cstdlib>
+
+template <typename MPTraits>
+class PointConstruction : public MPBaseObject<MPTraits> {
+    public:
+        PointConstruction();
+        PointConstruction(XMLNode& _node);
+
+        virtual void Initialize() override;
+
+
+        /// Sample on the surface of a sphere. (uses PMPL-defined Vector in mathtools)
+        /// Note: this does not sample INSIDE the sphere. 
+        /// @param center The D-dimensional Vector at which the sphere is centered. 
+        /// @param bounds The amount of distance in each dimension from the center to the surface of the sphere. 
+        ///                 In other words, bounds in each dimension if sphere is centered at (0, 0, ..., 0). 
+        ///                 When argument is a double, assume all bounds same distance (hypersphere with radius=bound)
+        /// @param nsamples The number of samples on the sphere surface. 
+        template <size_t D>
+            Vector<double, D> SampleSphereSurface(
+                Vector<double, D> center, Vector<double, D> bounds);
+        template <size_t D>
+            std::vector<Vector<double, D>> SampleSphereSurface(
+                Vector<double, D> center, Vector<double, D> bounds, int nsamples); 
+        template <size_t D>
+            std::vector<Vector<double, D>> SampleSphereSurface(
+                Vector<double, D> center, double bounds, int nsamples); 
+
+        /// Sample in a normal distribution. 
+        /// TODO: Move this somewhere else. 
+        double NormalDistribution(double mean, double variance);
+};
+
+template <typename MPTraits>
+PointConstruction<MPTraits>::
+PointConstruction() {
+  this->SetName("PointConstruction");
+}
+
+
+template <typename MPTraits>
+PointConstruction<MPTraits>::
+PointConstruction(XMLNode& _node) : MPBaseObject<MPTraits>(_node) {
+  this->SetName("PointConstruction");
+}
+
+template <typename MPTraits>
+void
+PointConstruction<MPTraits>::
+Initialize() {}
+
+template <typename MPTraits>
+template <size_t D>
+std::vector<Vector<double, D>> 
+PointConstruction<MPTraits>::
+SampleSphereSurface(Vector<double, D> center, 
+                    double bounds, 
+                    int nsamples) {
+
+    double b[D];
+    for (size_t i = 0; i<D; i++) { b[i] = bounds; }
+    Vector<double, D> _bounds(b);
+    return SampleSphereSurface(center, _bounds, nsamples);
+};
+
+template <typename MPTraits>
+template <size_t D>
+std::vector<Vector<double, D>> 
+PointConstruction<MPTraits>::
+SampleSphereSurface(Vector<double, D> center, 
+                    Vector<double, D> bounds, 
+                    int nsamples) {
+
+    std::vector<Vector<double, D>> samples;
+    for (int i = 0; i < nsamples; i++) {
+        samples.push_back(SampleSphereSurface(center, bounds));
+    }
+    
+    return samples;
+};
+
+template <typename MPTraits> 
+template <size_t D>
+Vector<double, D>
+PointConstruction<MPTraits>::
+SampleSphereSurface(Vector<double, D> center, Vector<double, D> bounds) {
+    // This follows the Muller Method (#3 and #19):
+    // http://extremelearning.com.au/how-to-generate-uniformly-random-points-on-n-spheres-and-n-balls/
+
+    double normal_samples[D];
+    for (size_t i = 0; i < D; i++) {
+        normal_samples[i] = NormalDistribution(0, 1);
+    }
+    Vector<double, D> samples(normal_samples);
+    if (this->m_debug) {
+        std::cout << "Normal distribution sample: " << normal_samples << std::endl;
+    }
+
+    samples = samples.selfNormalize();
+    samples = samples.elementwiseMultiply(bounds);
+    samples += center;
+    return samples;
+}
+
+template <typename MPTraits> 
+double 
+PointConstruction<MPTraits>::
+NormalDistribution(double mean, double variance) {
+    // TODO move to somewhere else
+    double std = sqrt(variance);
+    
+    int nStandardDeviations = 4;
+    double x = (DRand() * 2*nStandardDeviations*std) - (nStandardDeviations*std); // up to 3 standard deviations away
+
+    double constant = 1/(std * sqrt(2*PI));
+    double exponent = -0.5 * ((x-mean)*(x-mean)) / variance;
+
+    // std::cout << x << " " << constant * exp(exponent) << std::endl;
+    return constant * exp(exponent);
+}
+
+
+
+#endif
