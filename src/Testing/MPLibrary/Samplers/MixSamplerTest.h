b6bd65d2e95ca81d76d3595db535a87f47a452a4
SimonKato <sk106@illinois.edu>
2023-03-27 15:03:30 -0500

    MixSampler p-test finished

--- src/Testing/MPLibrary/Samplers/MixSamplerTest.h
+++ src/Testing/MPLibrary/Samplers/MixSamplerTest.h
@@ -3,6 +3,7 @@
 
 #include "MPLibrary/Samplers/MixSampler.h"
 #include "Testing/MPLibrary/Samplers/SamplerMethodTest.h"
+#include "Utilities/MPUtils.h"
 
 template <class MPTraits>
 class MixSamplerTest : virtual public MixSampler<MPTraits>, 
@@ -48,8 +49,15 @@ class MixSamplerTest : virtual public MixSampler<MPTraits>,
     ///@{
 
       void IndividualCfgSample(size_t& _numNodes, Boundary*& _boundary, 
-                    std::vector<Cfg>& _cfgs, std::vector<double>& _sampleProportions);
+                    std::vector<Cfg>& _cfgs, std::vector<size_t>& _sampleProportions);
     
+    ///@}
+    ///@Helpers
+    ///@{
+
+    double binom(int _n, int _k, double _p);
+
+    double multinom(int _n, std::vector<size_t>& _k, std::vector<double>& _p);
     ///@}
 
 
@@ -83,18 +91,56 @@ TestIndividualCfgSample() {
   Boundary* boundary = nullptr;
   std::vector<Cfg> cfgs;
 
+  // Sample using MixSampler a bunch of times. Keep a track of which type sampler was 
+  // used.
+
   // Make a vector to calculate the sample instances of each of the samplers.
-  std::vector<double> sampleProportions(this->samplers.size(), 0);
+  std::vector<size_t> sampleProportions(this->samplers.size(), 0);
 
-  size_t numNodes = 10000;
+  size_t numTrials= 500;
 
-  this->IndividualCfgSample(numNodes, boundary, cfgs, sampleProportions);
+  this->IndividualCfgSample(numTrials, boundary, cfgs, sampleProportions);
   
-  std::cout << cfgs.size() << std::endl;
+  // The test for the mix sampler is going to do "n" = the number of samplers 
+  // two-sided binomial p-test. If any of these fail, the mixsampler fails.
+  //    I think technically this is the wrong test because we have multiple classes. 
+  //    ie. we are checking if each class is within the acceptable bounds instead of
+  //    if the error from all the classes is within the acceptable bounds.
+
+  // Define the significance level
+  double alpha = 0.2;
 
   for(size_t i = 0; i < sampleProportions.size(); i++) {
-    std::cout << this->GetSampler(sampler.first) << ": " << sampleProportions[i]/(numNodes) << std::endl;
+    // n = numTrials
+    // k = sampleProportions[i];
+    double p = this->samplers[i].second; //probability of success
+
+    // Samplers.second contains the cummulative sums, so we need to subtract
+    if (i > 0) { p = p - this->samplers[i-1].second; }
+
+    // Conduct a binomial random variable p-test
+    bool tempTest = binom_test(alpha, numTrials, sampleProportions[i], p);
+    
+    if (!tempTest) { passed = false; }
   }
+
+  // Multinomial Statistical Test - overflowing
+  //std::vector<double> proportions;
+  //proportions.push_back(this->samplers[0].second);
+
+  //for (size_t j = 1; j < this->samplers.size(); j++) {
+  //  proportions.push_back(this->samplers[j].second - this->samplers[j-1].second);
+  //} 
+
+  //double criticalValue = multinom(numTrials, sampleProportions, proportions);
+  //double p_value = 0;
+
+  //std::vector<size_t> testProportions;
+  //for (size_t i = 0; i <= numTrials; i++) {
+  //  testProportions = {i, nuTrials - i};
+  //  double tempValue = multinom(numTrials, testProportions, proportions);
+  //  if (tempValue <= criticalValue) { p_value += tempValue; }
+  //}
 
   if(passed) {
     message = "IndividualCfgSample::PASSED!\n";
@@ -208,7 +254,7 @@ TestGroupFilter() {
 template <typename MPTraits>
 void
 MixSamplerTest<MPTraits>::
-IndividualCfgSample(size_t& _numNodes, Boundary*& _boundary, std::vector<Cfg>& _cfgs, std::vector<double>& _sampleProportions) {
+IndividualCfgSample(size_t& _numNodes, Boundary*& _boundary, std::vector<Cfg>& _cfgs, std::vector<size_t>& _sampleProportions) {
 
   this->SetLibraryRobot();
 
@@ -223,5 +269,32 @@ IndividualCfgSample(size_t& _numNodes, Boundary*& _boundary, std::vector<Cfg>& _
     int samplerIndex = this->samplerLabels.second;
     _sampleProportions[samplerIndex] += 1;
   }
+}
+
+template <typename MPTraits>
+double
+MixSamplerTest<MPTraits>::
+binom(int _n, int _k, double _p) {
+  double binomCoeff = boost::math::binomial_coefficient<double>(_n, _k);
+  double p_value = binomCoeff*std::pow(_p, _k)*std::pow(1-_p, _n - _k);
+  return p_value;
+}
+
+template <typename MPTraits>
+double
+MixSamplerTest<MPTraits>::
+multinom(int _n, std::vector<size_t>& _k, std::vector<double>& _p) {
+  double n_fact = boost::math::factorial<double>(_n);
+  std::cout << "n! good" << std::endl;
+  double prod_term = 1;
+  for (size_t j = 0; j < _k.size(); j++) {
+    double numerator = std::pow(_p[j], _k[j]);
+    std::cout << "power" << std::endl;
+    double denominator = boost::math::factorial<double>(_k[j]);
+    std::cout << "k!" << std::endl;
+    prod_term *= (numerator/denominator);
+  }
+
+  return n_fact*prod_term;
 }
 #endif
