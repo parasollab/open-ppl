33013d1ae99e6433b1723205882eff56dfc670b0
Stav <stava2@illinois.edu>
2023-04-06 17:09:30 -0500

    now collision checking only the path from query

--- src/MPLibrary/MapEvaluators/CollisionEvaluator.h
+++ src/MPLibrary/MapEvaluators/CollisionEvaluator.h
@@ -12,8 +12,11 @@ template <typename MPTraits>
 class CollisionEvaluator : public MapEvaluatorMethod<MPTraits> {
   public:
 
-    typedef typename MPTraits::CfgType               CfgType;
-    typedef typename MPTraits::RoadmapType           RoadmapType;
+    typedef typename MPTraits::CfgType          CfgType;
+    typedef typename MPTraits::RoadmapType      RoadmapType;
+    typedef typename RoadmapType::VID           VID;
+    typedef typename MPTraits::Path             Path;
+
 
     CollisionEvaluator();
     CollisionEvaluator(XMLNode& _node);
@@ -57,21 +60,46 @@ operator()() {
 
   vector<size_t> collisions;
 
-  for (auto eit = r->edges_begin(); eit != r->edges_end(); ++eit ){
-    evc->ValidateEdge(eit->source(), eit->target(), collisions);
+  // for (auto eit = r->edges_begin(); eit != r->edges_end(); ++eit ){
+  //   evc->ValidateEdge(eit->source(), eit->target(), collisions);
 
-    CfgType c1 = r->GetVertex(eit->source());
-    CfgType c2 = r->GetVertex(eit->target());
+  //   CfgType c1 = r->GetVertex(eit->source());
+  //   CfgType c2 = r->GetVertex(eit->target());
 
-    std::cout << std::setprecision(4);
+  //   std::cout << std::setprecision(4);
 
-    cout << "the edge (" << c1[0] << ", " << c1[1] <<", " << c1[2] <<") --> (" << c2[0] << ", " << c2[1] <<", " << c2[2] <<")" << endl;
+  //   cout << "the edge (" << c1[0] << ", " << c1[1] <<", " << c1[2] <<") --> (" << c2[0] << ", " << c2[1] <<", " << c2[2] <<")" << endl;
 
-    cout << "collides with obsts: ";
-    for (auto &col: collisions) {
-      cout << col << " ";
+  //   cout << "collides with obsts: ";
+  //   for (auto &col: collisions) {
+  //     cout << col << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  Path* path= this->GetPath();
+
+  if(path and path->Size()){
+    const vector<VID>& pathVIDs = path->VIDs();
+
+    for (size_t i = 0; i < pathVIDs.size() - 1; i++) {
+
+      evc->ValidateEdge(pathVIDs[i], pathVIDs[i+1], collisions);
+      
+      CfgType c1 = r->GetVertex(pathVIDs[i]);
+      CfgType c2 = r->GetVertex(pathVIDs[i+1]);
+
+      std::cout << std::setprecision(4);
+
+      cout << "the edge (" << c1[0] << ", " << c1[1] <<", " << c1[2] <<") --> (" << c2[0] << ", " << c2[1] <<", " << c2[2] <<")" << endl;
+
+      cout << "collides with obsts: ";
+      for (auto &col: collisions) {
+        cout << col << " ";
+      }
+      cout << endl;
+
     }
-    cout << endl;
   }
 
    return true;
