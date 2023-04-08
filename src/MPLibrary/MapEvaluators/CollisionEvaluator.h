56c98c60cb64006183c4984452e30e38b80ce520
Stav <stava2@illinois.edu>
2023-04-08 16:16:34 -0500

    Added path collision output

--- src/MPLibrary/MapEvaluators/CollisionEvaluator.h
+++ src/MPLibrary/MapEvaluators/CollisionEvaluator.h
@@ -25,7 +25,13 @@ class CollisionEvaluator : public MapEvaluatorMethod<MPTraits> {
 
   private:
 
+    /// Write the coliisions into a path collision (.pc) file.
+    /// @param _filename The name of the map file to write to.
+    void Write(const std::string& _filename) const;
+
     string m_evcLabel;               ///< the edge validity checker label
+
+    vector<size_t> m_collisions;     ///< Indices of obstacles found in collision
 
 };
 
@@ -55,17 +61,43 @@ operator()() {
 
   RoadmapType* r = this->GetRoadmap();
 
+  auto env = this->GetEnvironment();
+
+  size_t numObst = env->NumObstacles();
+
+  // Flags for obstacles in collision
+  vector<bool> collisionIndicators = vector<bool>(numObst, false);
+
   vector<size_t> collisions;
 
-  for (auto eit = r->edges_begin(); eit != r->edges_end(); ++eit ){
-    evc->ValidateEdge(eit->source(), eit->target(), collisions);
+  Path* path= this->GetPath();
 
-    CfgType c1 = r->GetVertex(eit->source());
-    CfgType c2 = r->GetVertex(eit->target());
+  bool pathExists = (path and path->Size());
 
-    std::cout << std::setprecision(4);
+  if(pathExists){
+    const vector<VID>& pathVIDs = path->VIDs();
 
-    cout << "the edge (" << c1[0] << ", " << c1[1] <<", " << c1[2] <<") --> (" << c2[0] << ", " << c2[1] <<", " << c2[2] <<")" << endl;
+    for (size_t i = 0; i < pathVIDs.size() - 1; i++) {
+
+      evc->ValidateEdge(pathVIDs[i], pathVIDs[i+1], collisions);
+      
+      CfgType c1 = r->GetVertex(pathVIDs[i]);
+      CfgType c2 = r->GetVertex(pathVIDs[i+1]);
+
+
+      for (auto &col: collisions) {
+        collisionIndicators[col] = true;
+      }
+
+      // std::cout << std::setprecision(4);
+
+      // cout << "the edge (" << c1[0] << ", " << c1[1] <<", " << c1[2] <<") --> (" << c2[0] << ", " << c2[1] <<", " << c2[2] <<")" << endl;
+
+      // cout << "collides with obsts: ";
+      // for (auto &col: collisions) {
+      //   cout << col << " ";
+      // }
+      // cout << endl;
 
     cout << "collides with obsts: ";
     for (auto &col: collisions) {
@@ -74,7 +106,31 @@ operator()() {
     cout << endl;
   }
 
-   return true;
+  for (size_t i = 0; i < numObst; i++){
+    if (collisionIndicators[i])
+      m_collisions.push_back(i);
+  }
+
+  const std::string base = this->GetBaseFilename();
+
+  Write(base + ".pc" );
+
+
+  return pathExists;
 }
+
+
+template <typename MPTraits>
+void
+CollisionEvaluator<MPTraits>::
+Write(const std::string& _filename) const {
+  std::ofstream ofs(_filename);
+  
+  for (const size_t& obstIdx: m_collisions)
+    ofs << obstIdx << " ";
+
+  ofs.close();
+}
+
 
 #endif
