87854fc3a13d1e8be99aa673fc526b2e9a12dcaa
Maria Lusardi <mclntg@mail.missouri.edu>
2024-02-08 10:32:30 -0600

    subset validity checker finished. dyn roadmap checker started

--- obstacleSubset/obst_subset.cpp
+++
@@ -1,8 +0,0 @@
-#include "MPLibrary/ValidityCheckers/CollisionDetectionValidity.h"
-
-int main(int argc, char const *argv[])
-{
-    CollisionDetectionValidity cdv = CollisionDetectionValidity();
-    print(cdv.GetEnvironment();)
-    return 0;
-}
